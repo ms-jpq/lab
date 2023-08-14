fe77bf55128a75d3af56719ca784b058e7c86fae
IIIIII <github@bigly.dog>
2023-08-13 22:12:48 -0700

    init21

--- makelib/facts.mk
+++ makelib/facts.mk
@@ -1,13 +1,9 @@
 .PHONY: facts
 
+MACHINES := $(shell printf -- '%s ' machines/*)
 INVENTORY := ./inventory.json
 ENV_SH := $(VAR)/sh/libexec/env.sh
 
-facts: $(INVENTORY)
-$(INVENTORY):
-	printf -- '%s\n' '{}' >'$@'
-
-$(ENV_SH): $(VAR)/sh
 
 ifeq ($(origin FACTS), command line)
 FACT_MACHINES := $(FACTS)
@@ -15,11 +11,26 @@ else
 FACT_MACHINES := $(shell ./libexec/inventory.sh --action ls --inventory '$(INVENTORY)')
 endif
 
+
+facts: $(INVENTORY)
+$(INVENTORY):
+	printf -- '%s\n' '{}' >'$@'
+
+$(ENV_SH): $(VAR)/sh
+
+
 define FACT_TEMPLATE
 facts: $(TMP)/machines/$1/env.json
 $(TMP)/machines/$1/env.json: ./libexec/inventory.sh $(ENV_SH) $(INVENTORY) | $(TMP)/machines/$1
 	'$$<' --action env --inventory '$(INVENTORY)' --machine '$1' -- '$(ENV_SH)' >'$$@'
-
 endef
 
+
+define NOFACT_TEMPLATE
+$(TMP)/machines/$1/env.json: $(INVENTORY) | $(TMP)/machines/$1
+	printf -- '%s\n' '{}' >'$$@'
+endef
+
+
 $(foreach machine,$(FACT_MACHINES),$(eval $(call FACT_TEMPLATE,$(machine))))
+$(foreach machine,$(filter-out $(FACT_MACHINES),$(patsubst machines/%,%,$(MACHINES))),$(eval $(call NOFACT_TEMPLATE,$(machine))))
--- makelib/local.mk
+++ makelib/local.mk
@@ -4,39 +4,9 @@ e :=
 s := ,
 sp := $(e) $(e)
 
-MACHINES := $(shell printf -- '%s ' machines/*)
 
 LSYNC := $(VAR)/sh/libexec/lsync.sh
 $(LSYNC): $(VAR)/sh
-
-
-define LOCAL_TEMPLATE
-LOCALS.$1 :=
-
-MACH.$1.LAYERS := layers/{$(subst $(sp),$(s),$(strip $(shell tr '\n' ' ' <'$1/layers.txt')))}
-MACH.$1.DIRS := $$(shell find $$(MACH.$1.LAYERS) -type d)
-MACH.$1.FILES := $$(shell find $$(MACH.$1.LAYERS) -type f,l)
-
-$(TMP)/$1/./: | $(TMP)/$1
-$(TMP)/$1: | $(TMP)
-	mkdir -v -p -- '$$@'
-$(TMP)/$1/layers/: | $(TMP)/$1
-	mkdir -v -p -- '$$@'
-
-
-$(TMP)/$1/facts.json: ./libexec/facts.sh $(TMP)/$1/env.json | $(TMP)/$1
-	'$$<' '$1' '$(TMP)/$1/env.json' >'$$@'
-
-
-$$(foreach layer,$$(MACH.$1.DIRS),$$(eval $$(call LOCAL_D_TEMPLATE,$1,$$(layer))))
-$$(foreach layer,$$(MACH.$1.FILES),$$(eval $$(call LOCAL_F_TEMPLATE,$1,$$(layer))))
-
-
-local: $(TMP)/$1/fs
-
-$(TMP)/$1/fs: $(VAR)/sh/libexec/lsync.sh $$(LOCALS.$1) | $(VAR)/sh
-	'$$<' '$$@' $(TMP)/$1/layers/*/
-endef
 
 
 define LOCAL_D_TEMPLATE
@@ -61,4 +31,53 @@ endif
 endef
 
 
+define LOCAL_L_TEMPLATE
+LOCALS.$1 += $(TMP)/$1/layers/_/$3
+
+$(VAR)/sh/$2: | $(VAR)/sh
+$(TMP)/$1/layers/_/$3: $(VAR)/sh/$2
+	mkdir -v -p -- '$$(@D)'
+	cp -v -P -f -- '$$<' '$$@'
+endef
+
+
+define LOCAL_TEMPLATE
+LOCALS.$1 :=
+
+MACH.$1.LAYERS := layers/{$(subst $(sp),$(s),$(strip _ $(shell tr '\n' ' ' <'$1/layers.txt')))}
+MACH.$1.DIRS := $$(shell find $$(MACH.$1.LAYERS) -type d)
+MACH.$1.FILES := $$(shell find $$(MACH.$1.LAYERS) -type f,l)
+
+$(TMP)/$1/./: | $(TMP)/$1
+$(TMP)/$1: | $(TMP)
+	mkdir -v -p -- '$$@'
+$(TMP)/$1/layers/: | $(TMP)/$1
+	mkdir -v -p -- '$$@'
+
+
+$(TMP)/$1/facts.json: ./libexec/facts.sh $(TMP)/$1/env.json | $(TMP)/$1
+	'$$<' '$1' '$(TMP)/$1/env.json' >'$$@'
+
+
+$$(foreach layer,$$(MACH.$1.DIRS),$$(eval $$(call LOCAL_D_TEMPLATE,$1,$$(layer))))
+$$(foreach layer,$$(MACH.$1.FILES),$$(eval $$(call LOCAL_F_TEMPLATE,$1,$$(layer))))
+$$(foreach line,$(REF_LINKS),$$(eval $$(call LOCAL_L_TEMPLATE,$1,$$(firstword $$(subst !, ,$$(line))),$$(lastword $$(subst !, ,$$(line))))))
+
+
+local: $(TMP)/$1/fs
+
+$(TMP)/$1/fs: $(VAR)/sh/libexec/lsync.sh $$(LOCALS.$1) | $(VAR)/sh
+	'$$<' '$$@' $(TMP)/$1/layers/*/
+endef
+
+
+define REF_LINKS
+
+layers/posix/home/.local/opt/initd/libexec/pkg.sh                         usr/local/opt/initd/libexec/pkg.sh
+layers/ubuntu/root/usr/local/lib/systemd/resolved.conf.d/0-override.conf  usr/local/lib/systemd/resolved.conf.d/0-override.conf
+layers/ubuntu/root/usr/local/lib/systemd/timesyncd.conf.d/0-override.conf usr/local/lib/systemd/timesyncd.conf.d/0-override.conf
+
+endef
+
+REF_LINKS := $(shell tr -s ' ' '!' <<<'$(REF_LINKS)')
 $(foreach machine,$(MACHINES),$(eval $(call LOCAL_TEMPLATE,$(machine))))
