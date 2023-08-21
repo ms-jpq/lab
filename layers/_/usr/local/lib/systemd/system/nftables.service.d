793e481e4563644237ae4c758da77902fe72604d
IIIIII <github@bigly.dog>
2023-08-20 19:49:55 -0700

    nft

---
+++ layers/_/usr/local/lib/systemd/system/nftables.service.d
@@ -0,0 +1,1 @@
+../../../opt/nftables/nftables.service.d
---
+++ layers/_/usr/local/opt/nginx/libexec/sidecar.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+RUN='/run/local/nginx'
+
+CONF_D=(
+  conf.d
+  http.d
+  location.d
+  stream.d
+  www
+)
+
+while true; do
+  TMP="$(mktemp --directory)"
+
+  for CONF in "${CONF_D[@]}"; do
+    mkdir -v --parents -- "$TMP/$CONF"
+  done
+
+  PIDS=()
+  for EXEC in "${0%/*}/../generators"/*; do
+    if [[ -x "$EXEC" ]]; then
+      "$EXEC" "$TMP" &
+      PIDS+=("$!")
+    fi
+  done
+  for PID in "${PIDS[@]}"; do
+    wait -- "$PID"
+  done
+
+  if diff --recursive -- "$TMP" "$RUN"; then
+    rsync --recursive --perms -- "$TMP/" "$RUN/"
+    systemctl reload -- nginx.service
+  fi
+
+  rm -v --recursive --force -- "$TMP"
+  sleep -- 5
+done
