c4621aaff3c8189f38284fb024956f5d9e6fe8a2
IIIIII <github@bigly.dog>
2023-08-26 20:45:24 -0700

    7

--- layers/printer/usr/local/opt/initd/makelib/printer.mk
+++ layers/printer/usr/local/opt/initd/makelib/printer.mk
@@ -1,11 +1,9 @@
 .PHONY: printer
 all: printer
 
-printer: /var/lib/local/cups/cupsd.conf
+printer: /usr/local/lib/cups/cupsd.conf
 
 /etc/cups/cupsd.conf: | pkg._
-/var/lib/local/cups/cupsd.conf: /usr/local/opt/cups/libexec/rewrite.sed /etc/cups/cupsd.conf
-	mkdir -v -p -- '$(@D)'
-	'$<' '/etc/cups/cupsd.conf' >'$@'
+/usr/local/lib/cups/cupsd.conf: /etc/cups/cupsd.conf | /usr/local/opt/cups/libexec/rewrite.sed
+	/usr/local/opt/cups/libexec/rewrite.sed '$<' | sudo -- sponge -- '$@'
 	sudo -- chgrp -- lpadmin '$@'
-	sudo -- touch -- '$@'
