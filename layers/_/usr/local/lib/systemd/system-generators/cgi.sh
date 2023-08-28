09bb6c08f6c2b08383226e764225cfd48649123a
IIIIII <github@bigly.dog>
2023-08-27 18:47:19 -0700

    cgi

--- layers/_/usr/local/lib/systemd/system-generators/cgi.sh
+++ layers/_/usr/local/lib/systemd/system-generators/cgi.sh
@@ -3,12 +3,12 @@
 set -o pipefail
 
 RUN="$1"
-WANTS="$RUN/socket.target.wants"
+WANTS="$RUN/sockets.target.wants"
 
 mkdir -v -p -- "$WANTS"
 for BIN in /usr/local/opt/cgi/bin/*; do
   if [[ -x "$BIN" ]]; then
-    NAME="${BIN##*/}"
+    NAME="6-cgi-${BIN##*/}"
     SOCK="$RUN/$NAME.socket"
     cp -v -f -- /usr/local/opt/cgi/6-cgi-@.service "$RUN/$NAME.service"
     cp -v -f -- /usr/local/opt/cgi/6-cgi-.socket "$SOCK"
