44e02c1eeb429cb617c602566a4e4d1d3088ad95
IIIIII <github@bigly.dog>
2023-08-28 05:41:25 -0700

    tt
    nn

--- layers/_/usr/local/opt/network/libexec/ula64.sh
+++ layers/_/usr/local/opt/network/libexec/ula64.sh
@@ -9,4 +9,4 @@ ULA=$((0xfd << SHIFT))
 B2="$(b2sum --binary --length "$SHIFT" <<<"$ID")"
 B2="${B2% *}"
 printf -v BITS -- '%x' $((ULA ^ "0x$B2"))
-exec -- perl -CASD -wpe 's/(.{4})(?=.)/\1:/g' <<<"$BITS"
+exec -- perl -CASD -wpe 's/(.{4})(?=.)/$1:/g' <<<"$BITS"
