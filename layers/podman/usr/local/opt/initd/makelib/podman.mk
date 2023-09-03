ee2fa40504a9465a7f8a9ff32353d7cbf0e988f7
IIIIII <github@bigly.dog>
2023-09-03 14:27:40 -0700

    minifl

--- layers/podman/usr/local/opt/initd/makelib/podman.mk
+++ layers/podman/usr/local/opt/initd/makelib/podman.mk
@@ -1,3 +1,8 @@
+.PHONY: clobber.podman
+
+clobber.podman:
+	podman system prune --all --force
+
 pkg._: /etc/apt/trusted.gpg.d/libcontainers.gpg
 /etc/apt/trusted.gpg.d/libcontainers.gpg:
 	$(CURL) -- 'https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(VERSION_ID)/Release.key' | sudo -- gpg --batch --dearmor --yes --output '$@'
