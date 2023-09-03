d5c0b93980778854e5c489f0c5ac6cbfa664917a
IIIIII <github@bigly.dog>
2023-09-03 13:41:07 -0700

    no modp

--- layers/vfio/usr/local/opt/vfio/bin/lspci.sh
+++ layers/vfio/usr/local/opt/vfio/bin/lspci.sh
@@ -2,18 +2,22 @@
 
 set -o pipefail
 
+/usr/local/libexec/hr.sh
+
 PREFIX=/sys/kernel/iommu_groups/
 for GROUP in "$PREFIX"*; do
   REL="${GROUP##"$PREFIX"}"
   printf -- '%s\n' "$REL"
 
   for DEVICE in "$GROUP"/devices/*; do
-    NAME="${DEVICE#*/}"
+    NAME="${DEVICE##*/}"
     PCI="$(lspci -nn -s "$NAME")"
 
     printf -- '  %s\n' "$PCI"
   done
 done
+
+/usr/local/libexec/hr.sh
 
 # Should see `vfio-pci`
 lspci -k
