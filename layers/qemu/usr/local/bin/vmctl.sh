99d3bda60c7fc1f1c47501a9be2039a39bc7a72d
IIIIII <github@bigly.dog>
2023-09-04 01:46:33 -0700

    tpmsock

---
+++ layers/qemu/usr/local/bin/vmctl.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+RUN='/run/local/qemu'
+
+ACTION="$1"
+MACHINE="$2"
+shift -- 2
+
+case "$ACTION" in
+c | con | console)
+  SOCK='con'
+  ;;
+m | mon | monitor)
+  SOCK='mon'
+  ;;
+q | qmp)
+  SOCK='con'
+  ;;
+*)
+  exit 2
+  ;;
+esac
+
+SOCKET="$RUN/$MACHINE/$SOCK.sock"
+exec -- socat 'READLINE' "UNIX-CONNECT:$SOCKET"
