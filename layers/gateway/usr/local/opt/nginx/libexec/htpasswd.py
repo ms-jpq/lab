22dff7c4aa4237ac10cb48edbf4a0b1e19255a9d
IIIIII <github@bigly.dog>
2023-09-05 14:10:18 -0700

    ok3

--- layers/gateway/usr/local/opt/nginx/libexec/htpasswd.py
+++ layers/gateway/usr/local/opt/nginx/libexec/htpasswd.py
@@ -19,32 +19,41 @@ from collections.abc import (
 )
 from contextlib import suppress
 from fnmatch import translate
-from functools import cache, lru_cache
+from functools import lru_cache
 from hmac import compare_digest, digest
 from http.cookies import CookieError, SimpleCookie
 from os.path import normcase
 from pathlib import Path, PurePosixPath
+from posixpath import commonpath
 from re import compile
 from time import time
-from typing import NewType, Pattern
+from typing import NewType
 from urllib.parse import parse_qs, urlsplit
 
 _Method = NewType("_Method", bytes)
 _Headers = NewType("_Headers", Mapping[bytes, Sequence[bytes]])
-_Queries = NewType("_Queries", Mapping[bytes, Sequence[bytes]])
-_Request = tuple[_Method, PurePosixPath, _Queries, _Headers]
+_Query = NewType("_Query", Mapping[bytes, Sequence[bytes]])
 
 _ALGORITHM = "sha512"
-_OK = b"HTTP/1.1 204 No Content\r\n\r\n"
-_DIE = b"HTTP/1.1 401 Unauthorized\r\n\r\n"
 
 
-async def _parse(reader: StreamReader) -> _Request:
+def _fnmatch(patterns: frozenset[str]) -> Callable[[str], bool]:
+    re = {compile(translate(pat)) for pat in patterns}
+
+    @lru_cache
+    def cont(host: str) -> bool:
+        return any(r.match(host) for r in re)
+
+    return cont
+
+
+async def _parse(
+    reader: StreamReader,
+) -> tuple[_Method, bytes, _Query, _Headers]:
     line = await anext(reader)
     method, path, _ = line.strip().split()
     parsed = urlsplit(path)
-    p = PurePosixPath(parsed.path.decode())
-    q = parse_qs(parsed.query)
+    query = parse_qs(parsed.query)
 
     headers: MutableMapping[bytes, MutableSequence[bytes]] = {}
     async for line in reader:
@@ -55,7 +64,7 @@ async def _parse(reader: StreamReader) -> _Request:
         else:
             break
 
-    return _Method(method), p, _Queries(q), _Headers(headers)
+    return _Method(method), parsed.path, _Query(query), _Headers(headers)
 
 
 def _auth_headers(headers: _Headers) -> Iterator[bytes]:
@@ -63,17 +72,6 @@ def _auth_headers(headers: _Headers) -> Iterator[bytes]:
         lhs, sep, rhs = auth.partition(b" ")
         if sep and lhs.lower() in {b"basic", b"bearer"}:
             yield rhs
-
-
-@cache
-def _translate(pattern: str) -> Pattern[str]:
-    re = translate(pattern)
-    return compile(re)
-
-
-@lru_cache
-def _fnmatch(host: str, patterns: frozenset[str]) -> bool:
-    return any(_translate(pattern).match(host) for pattern in patterns)
 
 
 def _decode(secret: bytes, crip: bytes) -> bytes:
@@ -92,24 +90,27 @@ def _encode(secret: bytes, plain: bytes) -> bytes:
     return crip
 
 
-def _read_auth_cookies(headers: _Headers, key: str, secret: bytes) -> bool:
+def _read_auth_cookies(headers: _Headers, name: str, secret: bytes) -> bool:
     for cs in headers.get(b"cookie", []):
         with suppress(CookieError):
-            cookies = SimpleCookie(cs.decode())
-            if morsel := cookies.get(key, None):
+            cookies = SimpleCookie[str](cs.decode())
+            if morsel := cookies.get(name, None):
                 crip = morsel.value.encode()
-                exp = _decode(secret, crip=crip).decode()
-                if time() < int(exp):
-                    return True
+                with suppress(ValueError):
+                    exp = _decode(secret, crip=crip).decode()
+                    if time() < int(exp):
+                        return True
+    else:
+        return False
 
-    return False
 
-
-def _write_auth_cookies(writer: StreamWriter, key: str, secret: bytes) -> None:
-    now = str(int(time())).encode()
+def _write_auth_cookies(
+    writer: StreamWriter, name: str, ttl: float, secret: bytes
+) -> None:
+    now = str(int(time() + ttl)).encode()
     crip = _encode(secret, plain=now)
-    cookie = SimpleCookie()
-    cookie[key] = crip.decode()
+    cookie = SimpleCookie[str]()
+    cookie[name] = crip.decode()
     writer.write(b"HTTP/1.1 204 No Content\r\n")
     writer.write(str(cookie).encode())
     writer.write(b"\r\n\r\n")
@@ -125,72 +126,44 @@ async def _subrequest(sock: Path, credentials: bytes) -> bool:
     return int(status) in range(200, 299)
 
 
-async def _login(
-    writer: StreamWriter,
-    sock: Path,
-    session_key: str,
-    secret: bytes,
-    query: _Queries,
-) -> None:
-    user = b"".join(query.get(b"username", ()))
-    passwd = b"".join(query.get(b"password", ()))
-    auth = b64encode(user + b":" + passwd)
-    authorized = await _subrequest(sock, credentials=auth)
-
-    if authorized:
-        _write_auth_cookies(writer, key=session_key, secret=secret)
-    else:
-        writer.write(_DIE)
-
-
-def _auth(
-    writer: StreamWriter,
-    session_key: str,
-    secret: bytes,
-    headers: _Headers,
-    allow_list: frozenset[str],
-) -> None:
-    host = b"".join(headers.get(b"host", ())).decode()
-    with suppress(ValueError):
-        if _fnmatch(host, allow_list):
-            writer.write(_OK)
-            return
-
-        if _read_auth_cookies(headers, key=session_key, secret=secret):
-            writer.write(_OK)
-            return
-
-    writer.write(_DIE)
-
-
 def _handler(
-    sock: Path, session_key: str, secret: bytes, allow_list: frozenset[str]
+    sock: Path,
+    authn_path: bytes,
+    cookie_name: str,
+    cookie_ttl: float,
+    hmac_secret: bytes,
+    allow_list: frozenset[str],
 ) -> Callable[[StreamReader, StreamWriter], Awaitable[None]]:
+    match = _fnmatch(allow_list)
+
     async def cont(reader: StreamReader, writer: StreamWriter) -> None:
         try:
             _, path, query, headers = await _parse(reader)
+            host = b"".join(headers.get(b"host", ())).decode()
+            if match(host) or _read_auth_cookies(
+                headers, name=cookie_name, secret=hmac_secret
+            ):
+                writer.write(b"HTTP/1.1 204 No Content\r\n\r\n")
+                return
 
             for auth in _auth_headers(headers):
                 if await _subrequest(sock=sock, credentials=auth):
-                    _write_auth_cookies(writer, key=session_key, secret=secret)
-                    break
-            else:
-                if path.relative_to("/login"):
-                    await _login(
-                        writer,
-                        sock=sock,
-                        session_key=session_key,
-                        secret=secret,
-                        query=query,
+                    _write_auth_cookies(
+                        writer, name=cookie_name, ttl=cookie_ttl, secret=hmac_secret
                     )
-                else:
-                    _auth(
-                        writer,
-                        session_key=session_key,
-                        secret=secret,
-                        headers=headers,
-                        allow_list=allow_list,
+                    return
+
+            if commonpath((authn_path, path)) == authn_path:
+                user = b"".join(query.get(b"username", ()))
+                passwd = b"".join(query.get(b"password", ()))
+                auth = b64encode(user + b":" + passwd)
+                if await _subrequest(sock, credentials=auth):
+                    _write_auth_cookies(
+                        writer, name=cookie_name, ttl=cookie_ttl, secret=hmac_secret
                     )
+                    return
+
+            writer.write(b"HTTP/1.1 401 Unauthorized\r\n\r\n")
         finally:
             await writer.wait_closed()
 
@@ -201,25 +174,32 @@ def _parse_args() -> Namespace:
     parser = ArgumentParser()
     parser.add_argument("--listening-socket", type=Path, required=True)
     parser.add_argument("--htpasswd-socket", type=Path, required=True)
-    parser.add_argument("--session-key", required=True)
-    parser.add_argument("--allow-list", type=Path, required=True)
-    parser.add_argument("--secret", type=Path, required=True)
+    parser.add_argument("--cookie-name", required=True)
+    parser.add_argument("--cookie-ttl", type=float, required=True)
+    parser.add_argument("--allow-list", type=Path, nargs="*")
+    parser.add_argument("--hmac-secret", type=Path, required=True)
+    parser.add_argument("--authn-path", type=PurePosixPath, required=True)
     return parser.parse_args()
 
 
 async def main() -> None:
     args = _parse_args()
-    secret = Path(args.secret).read_bytes()
+    hmac_secret = Path(args.hmac_secret).read_bytes()
     allow_list = frozenset(
         line for line in Path(args.allow_list).read_text().splitlines() if line
     )
+    authn_path = PurePosixPath(args.authn_path).as_posix().encode()
+    assert authn_path.startswith(b"/")
+
     handler = _handler(
         args.htpasswd_socket,
-        session_key=args.session_key,
-        secret=secret,
+        cookie_name=args.cookie_name,
+        authn_path=authn_path,
+        cookie_ttl=args.ttl,
+        hmac_secret=hmac_secret,
         allow_list=allow_list,
     )
-    server = await start_unix_server(handler, normcase(args.socket))
+    server = await start_unix_server(handler, normcase(args.listening_socket))
 
     async with server:
         await server.serve_forever()
