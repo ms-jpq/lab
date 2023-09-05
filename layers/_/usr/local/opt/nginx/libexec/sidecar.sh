791b4cddc48dbdd4dbad627080a03b4ff5a4eee1
IIIIII <github@bigly.dog>
2023-09-04 19:41:31 -0700

    nginxsidecar

--- layers/_/usr/local/opt/nginx/libexec/sidecar.sh
+++ layers/_/usr/local/opt/nginx/libexec/sidecar.sh
@@ -4,37 +4,20 @@ set -o pipefail
 
 RUN="$1"
 WWW="$2"
+TMP="$3"
 
-CONF_D=(
-  conf.d
-  http.d
-  server.d
-  stream.d
-)
-
-TMP="$(mktemp --directory)"
-
-while true; do
-  for CONF in "${CONF_D[@]}"; do
-    mkdir --parents -- "$TMP/$CONF"
-  done
-
-  PIDS=()
-  for EXEC in "${0%/*}/../generators"/*; do
-    if [[ -x "$EXEC" ]]; then
-      "$EXEC" "$TMP" "$WWW" &
-      PIDS+=("$!")
-    fi
-  done
-  for PID in "${PIDS[@]}"; do
-    wait -- "$PID"
-  done
-
-  if ! diff --recursive --brief -- "$TMP" "$RUN"; then
-    rsync --recursive --perms -- "$TMP/" "$RUN/"
-    systemctl reload -- nginx.service
+PIDS=()
+for EXEC in "${0%/*}/../generators"/*; do
+  if [[ -x "$EXEC" ]]; then
+    "$EXEC" "$TMP" "$WWW" &
+    PIDS+=("$!")
   fi
-
-  rm --recursive --force -- "${TMP:?}"/*
-  sleep -- 5
 done
+for PID in "${PIDS[@]}"; do
+  wait -- "$PID"
+done
+
+if ! diff --recursive --brief -- "$TMP" "$RUN"; then
+  rsync --recursive --perms -- "$TMP/" "$RUN/"
+  systemctl reload -- nginx.service
+fi
