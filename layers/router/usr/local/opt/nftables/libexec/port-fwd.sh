a6019048d535da9547dc7fa8e092d6afb5b3051e
IIIIII <github@bigly.dog>
2023-09-06 22:03:46 -0700

    portfwd.sh

--- layers/router/usr/local/opt/nftables/libexec/port-fwd.sh
+++ layers/router/usr/local/opt/nftables/libexec/port-fwd.sh
@@ -1,73 +1,49 @@
-#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O globstar
 
 set -o pipefail
 
-# readarray -t -d $'\n' -- LINES < <(jq -r 'to_entries[] | "\(.key) \(.value | to_entries[] | "\(.key) . \(.value[])")"' <<<"$PORTS")
+RUN="$1"
+NFT="${0%/*}/../forwar.d"
+SCRIPT="$RUN/tr.sed"
+OLD="$RUN/old.conf"
+NEW="$RUN/new.conf"
 
-# declare -A -- SPEC
+SED=()
+for FILE in /run/local/dnsmasq/*/leases; do
+  DOMAIN="${FILE%/*}"
+  DOMAIN="${DOMAIN##*/}"
+  LS="$(awk '{ print($1, $3, $4) }' "$FILE")"
+  readarray -t -- LSS <<<"$LS"
 
-# for LINE in "${LINES[@]}"; do
-#   NAME="${LINE%% *}"
-#   NAME="${NAME,,}."
-#   PORT="${LINE#* }"
-#   SPEC[$NAME]="$PORT"$'\n'"${SPEC["$NAME"]:-}"
-# done
+  for LINE in "${LSS[@]}"; do
+    EXP="${LINE%% *}"
+    if [[ "$EXP" == 'duid' ]]; then
+      continue
+    fi
 
-# declare -A -- SEEN
+    LINE="${LINE#"$EXP" }"
+    IP="${LINE%% *}"
+    NAME="${LINE#"$IP" }"
+    if [[ "$NAME" == '*' ]]; then
+      continue
+    fi
 
-# add() {
-#   DOMAIN="$1"
-#   PORT="$2"
-#   IP="$3"
-#   SVC="${PORT#* }"
+    if [[ "$IP" =~ : ]]; then
+      SED+=("s/'$NAME.$DOMAIN.6'/$IP/p")
+    else
+      SED+=("s/'$NAME.$DOMAIN.4'/$IP/p")
+    fi
+  done
+done
 
-#   if [[ "$IP" =~ : ]]; then
-#     if [[ "$IP" =~ ^fd ]]; then
-#       if [[ -z "${SEEN["$PORT"]:-}" ]]; then
-#         SEEN["$PORT"]=1
-#         printf -- '%s\n' "add element inet user fw_v6 { $PORT : $IP $SVC }"
-#       fi
-#     else
-#       printf -- '%s\n' "add element inet user pass_v6 { $PORT . $IP }"
-#     fi
-#   else
-#     printf -- '%s\n' "add element inet user fw_v4 { $PORT : $IP $SVC }"
-#   fi
+printf -- '%s\n' "${SED[@]}" >"$SCRIPT"
 
-# }
+{
+  cat -- "$NFT/0-flush.conf"
+  sed --regexp-extended --quiet --file "$SCRIPT" "$NFT/1-elements.conf"
+} >"$NEW"
 
-# while true; do
-#   T1="$(mktemp --directory)"
-#   for DOMAIN in "${!SPEC[@]}"; do
-#     dig +short "$DOMAIN" AAAA "$DOMAIN" A >"$T1/$DOMAIN" &
-#   done
-
-#   if wait; then
-#     :
-#   fi
-
-#   SEEN=()
-#   T2="$(mktemp)"
-#   tee -- "$T2" >/dev/null <<-'EOF'
-# flush set inet user pass_v4
-# flush set inet user pass_v4
-# flush map inet user fw_v4
-# flush map inet user fw_v6
-# EOF
-#   for DOMAIN in "${!SPEC[@]}"; do
-#     readarray -t -d $'\n' -- IPS <"$T1/$DOMAIN"
-#     readarray -t -d $'\n' -- MAPPING <<<"${SPEC["$DOMAIN"]}"
-
-#     for PORT in "${MAPPING[@]}"; do
-#       if [[ -n "$PORT" ]]; then
-#         for IP in "${IPS[@]}"; do
-#           add "$DOMAIN" "$PORT" "$IP"
-#         done
-#       fi
-#     done
-#   done >>"$T2"
-
-#   nft --file "$T2"
-#   rm --recursive --force -- "$T1" "$T2"
-#   sleep -- 60
-# done
+if ! diff --brief -- "$OLD" "$NEW"; then
+  mv -v -f -- "$NEW" "$OLD"
+  nft --file "$OLD"
+fi
