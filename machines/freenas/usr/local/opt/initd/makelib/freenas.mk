0456fa777c85fa3dfd47ada2c05dad61acc7ccfa
IIIIII <github@bigly.dog>
2023-09-07 23:44:03 -0700

    todo

---
+++ machines/freenas/usr/local/opt/initd/makelib/freenas.mk
@@ -0,0 +1,3 @@
+pkg._: /etc/apt/sources.list.d/ppa_qbittorrent-team_qbittorrent-stable.list
+/etc/apt/sources.list.d/ppa_qbittorrent-team_qbittorrent-stable.list:
+	sudo -- ./libexec/add-ppa.sh qbittorrent-team/qbittorrent-stable
