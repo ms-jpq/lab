1b1da73266484fd55e7396a6408adc5e46d269af
IIIIII <github@bigly.dog>
2023-09-10 03:41:00 -0700

    CHECKPOINT

--- layers/_/usr/local/opt/nginx/journald/index.js
+++ layers/_/usr/local/opt/nginx/journald/index.js
@@ -68,84 +68,64 @@ const debounce = ((handle) => {
   };
 })(NaN);
 
-const append = (() => {
-  let anchor = undefined;
-  let intersecting = true;
-  const obs = new IntersectionObserver((entries) => {
-    for (const entry of entries) {
-      intersecting = entry.isIntersecting;
+/**
+ * @param {symbol} sym
+ * @param {HTMLElement} root
+ * @param {Record<string, string>} json
+ */
+const append = (sym, root, json) => {
+  const { [sym]: err, __REALTIME_TIMESTAMP, MESSAGE, SYSLOG_IDENTIFIER } = json;
+
+  const ts = __REALTIME_TIMESTAMP
+    ? new Date(Number(__REALTIME_TIMESTAMP) / 1000)
+    : new Date();
+  const li = document.createElement("li");
+
+  const tt =
+    ts.getFullYear().toString().slice(2) +
+    "/" +
+    (ts.getMonth() + 1).toString().padStart(2, 0) +
+    "/" +
+    ts.getDate().toString().padStart(2, 0) +
+    " " +
+    ts.getHours().toString().padStart(2, 0) +
+    ":" +
+    ts.getMinutes().toString().padStart(2, 0) +
+    ":" +
+    ts.getSeconds().toString().padStart(2, 0);
+  const time = document.createElement("time");
+  time.setAttribute("datetime", ts.toISOString());
+  time.appendChild(document.createTextNode(tt));
+
+  const id = document.createTextNode(SYSLOG_IDENTIFIER);
+  const b = document.createElement("b");
+  b.appendChild(id);
+
+  const msg = document.createTextNode(err ?? MESSAGE);
+  const line = err
+    ? (() => {
+        const em = document.createElement("em");
+        em.appendChild(msg);
+        return em;
+      })()
+    : msg;
+
+  const label = document.createElement("label");
+  label.appendChild(b);
+  label.appendChild(line);
+
+  li.appendChild(time);
+  li.appendChild(label);
+  root.appendChild(li);
+
+  debounce(() => {
+    const eof = document.querySelector("input")?.checked;
+    if (eof) {
+      root.scrollIntoView({ block: "end" });
+      intersecting = true;
     }
-  }, {});
-
-  /**
-   * @param {symbol} sym
-   * @param {HTMLElement} root
-   * @param {Record<string, string>} json
-   */
-  return (sym, root, json) => {
-    const {
-      [sym]: err,
-      __REALTIME_TIMESTAMP,
-      MESSAGE,
-      SYSLOG_IDENTIFIER,
-    } = json;
-
-    const ts = __REALTIME_TIMESTAMP
-      ? new Date(Number(__REALTIME_TIMESTAMP) / 1000)
-      : new Date();
-    const li = document.createElement("li");
-
-    const tt =
-      ts.getFullYear().toString().slice(2) +
-      "/" +
-      (ts.getMonth() + 1).toString().padStart(2, 0) +
-      "/" +
-      ts.getDate().toString().padStart(2, 0) +
-      " " +
-      ts.getHours().toString().padStart(2, 0) +
-      ":" +
-      ts.getMinutes().toString().padStart(2, 0) +
-      ":" +
-      ts.getSeconds().toString().padStart(2, 0);
-    const time = document.createElement("time");
-    time.setAttribute("datetime", ts.toISOString());
-    time.appendChild(document.createTextNode(tt));
-
-    const id = document.createTextNode(SYSLOG_IDENTIFIER);
-    const b = document.createElement("b");
-    b.appendChild(id);
-
-    const msg = document.createTextNode(err ?? MESSAGE);
-    const line = err
-      ? (() => {
-          const em = document.createElement("em");
-          em.appendChild(msg);
-          return em;
-        })()
-      : msg;
-
-    const label = document.createElement("label");
-    label.appendChild(b);
-    label.appendChild(line);
-
-    li.appendChild(time);
-    li.appendChild(label);
-    root.appendChild(li);
-
-    if (anchor) {
-      obs.unobserve(anchor);
-    }
-    anchor = li;
-    obs.observe(anchor);
-
-    debounce(() => {
-      if (intersecting) {
-        root.scrollIntoView({ block: "end" });
-        intersecting = true;
-      }
-    });
-  };
-})();
+  });
+};
 
 (async () => {
   const uri =
@@ -153,10 +133,6 @@ const append = (() => {
 
   const sym = Symbol();
   const root = globalThis?.document?.querySelector("ol");
-
-  globalThis?.document
-    ?.querySelector("button")
-    ?.addEventListener("click", () => root.scrollIntoView({ block: "end" }));
 
   for await (const json of stream(sym, uri)) {
     if (!root) {
