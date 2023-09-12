ba5e0dc860ea7c97bfd91527f00411b3acfa21eb
IIIIII <github@bigly.dog>
2023-09-12 07:13:14 -0700

    sanoid

---
+++ layers/certbot/usr/local/opt/nginx/generators/certbot.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O globstar
+
+set -o pipefail
+
+RUN="$1"
+WWW="$2"
+SELF="${0%/*}"
+CGI='/run/local/cgi/'
+
+HREFS=()
+SOCKS=()
+
+for SOCK in "$CGI"*.sock; do
+  BASE="${SOCK#"$CGI"}"
+  BASE="${BASE%'.sock'}"
+  HREFS+=("$BASE")
+  SOCKS+=("$BASE:$SOCK")
+done
+
+for TXT in "$SELF"/*.index; do
+  readarray -t -- INDEX <"$TXT"
+  HREFS+=("${INDEX[@]}")
+done
+
+IFS=','
+/usr/local/libexec/m4.sh -D"ENV_HOST=$HOSTNAME" -D"ENV_HREFS=${HREFS[*]}" "$SELF/index.html" | sponge -- "$WWW/index.html"
+/usr/local/libexec/m4.sh -D"ENV_SOCKS=${SOCKS[*]}" '/usr/local/opt/cgi/location.nginx' >"$RUN/server.d/cgi.nginx"
+unset -- IFS
