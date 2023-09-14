553c388e64eab4d4d0325a2c74ad84c7d67d6583
IIIIII <github@bigly.dog>
2023-09-14 01:50:46 -0700

    fallback

--- layers/gateway/usr/local/opt/nginx/generators/certbot.sh
+++ layers/gateway/usr/local/opt/nginx/generators/certbot.sh
@@ -3,3 +3,13 @@
 set -o pipefail
 
 RUN="$1"
+SSL="$RUN/ssl"
+mkdir -p -- "$SSL"
+
+readarray -t -- SITES </usr/local/etc/default/certbot.env
+for SITE in "${SITES[@]}"; do
+  NAME="${SITE%%=*}"
+  if [[ -n "$NAME" ]]; then
+    cp -f -- "/var/lib/local/certbot/nginx/$NAME.nginx" "$SSL/$NAME.nginx"
+  fi
+done
