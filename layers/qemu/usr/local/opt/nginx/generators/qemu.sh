959b9c37acb25ba2b412efade20b4396de2d7451
IIIIII <github@bigly.dog>
2023-09-17 02:44:50 -0700

    href

--- layers/qemu/usr/local/opt/nginx/generators/qemu.sh
+++ layers/qemu/usr/local/opt/nginx/generators/qemu.sh
@@ -4,21 +4,21 @@ set -o pipefail
 
 RUN="$1"
 WWW="$2"
-VNC='/run/local/qemu/'
+VNC='/run/local/vnc/'
 
 HREFS=()
 SOCKS=()
 
-S='/ws.sock'
-for SOCK in "$VNC"*"$S"; do
+for SOCK in "$VNC"*.sock; do
   BASE="${SOCK#"$VNC"}"
-  BASE="qemu/${BASE%"$S"}"
+  MACH="${BASE%.sock}"
+  BASE="qemu/$MACH"
   PARAMS="path=/$BASE/websocketify&reconnect=1&reconnect_delay=200&scale=1"
-  HREFS+=("$BASE/vnc_lite.html?$PARAMS")
+  HREFS+=("$BASE/vnc_lite.html?$PARAMS#$MACH")
   SOCKS+=("$BASE:$SOCK")
 done
 
 IFS=','
-/usr/local/libexec/m4.sh -D"ENV_HOST=$HOSTNAME" -D"ENV_HREFS=${HREFS[*]}" /usr/local/opt/nginx/generators/index.html | sponge -- "$WWW/qemu.html"
+/usr/local/libexec/m4.sh -D"ENV_HOST=$HOSTNAME" -D"ENV_HREFS=${HREFS[*]}" "${0%/*}/qemu.html" | sponge -- "$WWW/qemu.html"
 /usr/local/libexec/m4.sh -D"ENV_SOCKS=${SOCKS[*]}" /usr/local/opt/qemu/location.nginx >"$RUN/server.d/qemu.nginx"
 unset -- IFS
