0a2138273e7cc536d33fbc0166162acb7469f225
IIIIII <github@bigly.dog>
2023-09-18 13:29:19 -0700

    CHECKPOINT OWN NETWORK

--- layers/nspawn/usr/local/opt/nspawn/libexec/systemd-nspawn@.sh
+++ layers/nspawn/usr/local/opt/nspawn/libexec/systemd-nspawn@.sh
@@ -8,4 +8,16 @@ ROOT="$2"
 "${0%/*}/apriori.sh" "$MACHINE" "$ROOT"
 CONF='/usr/local/opt/nspawn/conf.d'
 cat -- "$CONF"/*.nspawn "$ROOT"/*.nspawn | envsubst | sponge -- "/run/systemd/nspawn/$MACHINE.nspawn"
-exec -- systemd-nspawn --keep-unit --boot --notify-read=yes -U --network-veth --link-journal=try-guest --settings=override --machine="$MACHINE" --directory="$ROOT/fs"
+ARGV=(
+  systemd-nspawn
+  --keep-unit
+  --boot
+  --notify-read yes
+  -U
+  --network-veth
+  --link-journal try-guest
+  --settings override
+  --machine "$MACHINE"
+  --directory "$ROOT/fs"
+)
+exec -- "${ARGV[@]}"
