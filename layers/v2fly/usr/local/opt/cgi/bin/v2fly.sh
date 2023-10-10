baea3971dc2c012790a225c68acf4b89c48bbaea
IIIIII <github@bigly.dog>
2023-10-10 01:52:44 -0700

    link

---
+++ layers/v2fly/usr/local/opt/cgi/bin/v2fly.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+if [[ ! -t 1 ]]; then
+  exec >&3
+fi
+
+tee -- <<-'EOF'
+HTTP/1.0 200 OK
+Content-Type: text/plain; charset=utf-8
+
+EOF
+
+A=(/usr/local/libexec/hr-run.sh /usr/local/libexec/v2ray api stats -server localhost:30000)
+
+"${A[@]}"
+"${A[@]}" -runtime
