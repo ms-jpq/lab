8ea9e70eb373d8da95830bfbd3b52395b1f9bd25
IIIIII <github@bigly.dog>
2023-10-12 21:00:16 -0700

    r2

--- layers/_/usr/local/opt/nftables/libexec/tproxy.sh
+++ layers/_/usr/local/opt/nftables/libexec/tproxy.sh
@@ -2,10 +2,13 @@
 
 set -o pipefail
 
-MARK=0xb00b0069
+MARKS=(0xb00b0069 0x69)
 TABLE=100
 
-ip rule add fwmark "$MARK" lookup "$TABLE"
 ip route add local default dev lo table "$TABLE"
-ip -6 rule add fwmark "$MARK" lookup "$TABLE"
 ip -6 route add local default dev lo table "$TABLE"
+
+for MARK in "${MARKS[@]}"; do
+  ip rule add fwmark "$MARK" lookup "$TABLE"
+  ip -6 rule add fwmark "$MARK" lookup "$TABLE"
+done
