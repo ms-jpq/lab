d6d0b31eaa4c87c17eca553b2cb74786cc47b372
IIIIII <github@bigly.dog>
2023-10-15 08:08:03 -0700

    cupsd

--- layers/printer/usr/local/opt/cups/libexec/rewrite.sed
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env -S -- sed -E -f
-
-s/^Listen localhost:([[:digit:]]+)$/Listen \1/g
-s/(AuthType) .+$/\1 None/g
-s/^Browsing .+$/Browsing Yes/g
--- layers/printer/usr/local/opt/initd/makelib/printer.mk
+++ layers/printer/usr/local/opt/initd/makelib/printer.mk
@@ -1,15 +1,12 @@
 .PHONY: printer
 all: printer
 
-printer: /usr/local/lib/cups/cupsd.conf
+/etc/cups/cupsd.conf: | pkg._
+
+printer: /home/cups
 printer.clobber:
 	sudo -- userdel --remove -- cups
 
-/home/cups:
+/home/cups: | /etc/cups/cupsd.conf
 	PASSWORD="$$(openssl passwd -1 -- '$(@F)')"
 	sudo -- useradd --create-home --gid lpadmin --password "$$PASSWORD" -- '$(@F)'
-
-/etc/cups/cupsd.conf: | pkg._
-/usr/local/lib/cups/cupsd.conf: /etc/cups/cupsd.conf | /usr/local/opt/cups/libexec/rewrite.sed /home/cups
-	sudo -- cp -- '$<' '$@'
-	sudo -- chown -- lp:lp /usr/local/lib/cups/**/*
