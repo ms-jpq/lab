53d9805b03fa6282a37adeb171add50eb90ab897
IIIIII <github@bigly.dog>
2023-11-12 21:03:10 +0800

    male

--- layers/mail/usr/local/opt/initd/makelib/mail.mk
+++ layers/mail/usr/local/opt/initd/makelib/mail.mk
@@ -5,5 +5,9 @@ mail: /var/lib/local/vmail
 mail.clobber:
 	sudo -- userdel --remove -- vmail
 
-/var/lib/local/vmail: | pkg._
-	sudo -- useradd --create-home --home-dir '$@' -- '$(@F)'
+/home/vmail: | pkg._
+	sudo -- useradd --create-home -- '$(@F)'
+
+/var/lib/local/vmail: | /home/vmail
+	sudo -- mkdir --parents -- '$@'
+	sudo -- chown -- '$(@F):$(@F)' '$@'
