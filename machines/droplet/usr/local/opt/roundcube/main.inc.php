e9083f05c31749992b52e10b0f1117d6498859be
IIIIII <github@bigly.dog>
2023-11-25 02:35:46 +0800

    roundcube
    roundcube

---
+++ machines/droplet/usr/local/opt/roundcube/main.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$config["imap_conn_options"] = [
+  "ssl" => ["verify_peer" => false, "verify_peer_name" => false],
+  "tls" => ["verify_peer" => false, "verify_peer_name" => false],
+];
+
+?>
