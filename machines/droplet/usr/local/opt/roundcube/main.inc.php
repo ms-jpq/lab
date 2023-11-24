93a1d1630941c7079616a5aa105e76071a0de26a
IIIIII <github@bigly.dog>
2023-11-25 03:21:20 +0800

    self-signed

--- machines/droplet/usr/local/opt/roundcube/main.inc.php
+++ machines/droplet/usr/local/opt/roundcube/main.inc.php
@@ -1,8 +1,7 @@
 <?php
 
-$config["imap_conn_options"] = [
-  "ssl" => ["verify_peer" => false, "verify_peer_name" => false],
-  "tls" => ["verify_peer" => false, "verify_peer_name" => false],
+$config["imap_conn_options"] = $config["smtp_conn_options"] = [
+  "ssl" => ["allow_self_signed" => true, "verify_peer_name" => false],
 ];
 
 ?>
