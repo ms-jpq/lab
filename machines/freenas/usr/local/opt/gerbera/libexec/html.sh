4abad0c1607d2c84d7eb607ecfc0157ac56d0003
IIIIII <github@bigly.dog>
2023-12-06 23:26:47 +0800

    REMOVE GERBERA

--- machines/freenas/usr/local/opt/gerbera/libexec/html.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
-
-set -o pipefail
-
-HTML='/var/lib/gerbera/gerbera.html'
-if [[ -f "$HTML" ]]; then
-  exec -- sed -E -n -- 's/.+URL=([^"]+).+/\1/p' "$HTML"
-fi
--- machines/freenas/usr/local/opt/initd/makelib/gerbera.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: gerbera
-
-gerbera: /usr/local/opt/gerbera/config.xml
-all: gerbera
-
-/etc/gerbera/config.xml: pkg._
-/usr/local/opt/gerbera/config.xml: /usr/local/opt/gerbera/libexec/xml.sh /etc/gerbera/config.xml
-	sudo -- /usr/local/libexec/sponge2.sh '$@' '$<' /etc/gerbera/config.xml
