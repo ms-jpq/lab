a0e4d6843d602c5aa710ee56642fe58f67d8702f
IIIIII <github@bigly.dog>
2023-12-06 20:53:07 +0800

    g3

--- machines/droplet/usr/local/opt/gerbera/libexec/xml.sh
+++ machines/droplet/usr/local/opt/gerbera/libexec/xml.sh
@@ -4,13 +4,18 @@ set -o pipefail
 
 cd -- "${0%/*}"
 
+PORT='49153'
+
 XML=(
   xmlstarlet edit
   -N 'x=http://mediatomb.cc/config/2'
   --update '/x:config/x:server/x:ui/@enabled' --value 'yes'
-  --update '/x:config/x:server/x:home' --value '/var/lib/local/gerbera'
-  --subnode '/x:config/x:server' --type elem --name 'interface' --value 'lo'
-  --subnode '/x:config/x:server' --type elem --name 'ip' --value '127.0.0.53'
+  # --update '/x:config/x:server/x:home' --value '/var/lib/local/gerbera'
+  # --subnode '/x:config/x:server' --type elem --name 'interface' --value '!lo'
+  --subnode '/x:config/x:server' --type elem --name 'port' --value "$PORT"
+  --subnode '/x:config/x:server' --type elem --name 'virtualURL' --value "http://localhost:$PORT"
+  --subnode '/x:config/x:server' --type elem --name 'pc-directory'
+  --subnode '/x:config/x:server/pc-directory' --type attr --name 'upnp-hide' --value 'no'
 )
 
 exec -- "${XML[@]}" "$@"
