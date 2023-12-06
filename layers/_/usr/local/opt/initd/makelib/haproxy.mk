32c927a47019742cc1f018af369cfa84d1de84d3
IIIIII <github@bigly.dog>
2023-12-06 12:33:05 +0800

    ok2

--- layers/_/usr/local/opt/initd/makelib/haproxy.mk
+++ layers/_/usr/local/opt/initd/makelib/haproxy.mk
@@ -8,5 +8,5 @@ all: haproxy
 /usr/local/opt/haproxy/haproxy.cfg: /usr/local/opt/haproxy/libexec/cfg.sed /etc/haproxy/haproxy.cfg
 	sudo -- /usr/local/libexec/sponge2.sh '$@' '$<' /etc/haproxy/haproxy.cfg
 
-/usr/local/opt/haproxy/gen.cfg: $(shell shopt -u failglob && printf -- '%s ' /usr/local/opt/haproxy/conf.d/*.cfg)
+/usr/local/opt/haproxy/gen.cfg: $(shell shopt -u failglob && printf -- '%s ' /usr/local/opt/haproxy/conf.d/*.cfg) | /etc/haproxy/haproxy.cfg
 	sudo -- /usr/local/libexec/sponge2.sh '$@' cat -- /dev/null '$^'
