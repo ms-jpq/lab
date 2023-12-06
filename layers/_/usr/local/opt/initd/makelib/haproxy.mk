b442e7a065b448df5a466fac3eb8eec3d6863597
IIIIII <github@bigly.dog>
2023-12-06 12:26:20 +0800

    sudo!

--- layers/_/usr/local/opt/initd/makelib/haproxy.mk
+++ layers/_/usr/local/opt/initd/makelib/haproxy.mk
@@ -6,7 +6,7 @@ all: haproxy
 /etc/haproxy/haproxy.cfg: | pkg._
 
 /usr/local/opt/haproxy/haproxy.cfg: /usr/local/opt/haproxy/libexec/cfg.sed /etc/haproxy/haproxy.cfg
-	'$<' /etc/haproxy/haproxy.cfg >'$@'
+	sudo -- '$<' /etc/haproxy/haproxy.cfg >'$@'
 
 /usr/local/opt/haproxy/gen.cfg: $(shell shopt -u failglob && printf -- '%s ' /usr/local/opt/haproxy/conf.d/*.cfg)
-	cat -- /dev/null '$^' >'$@'
+	sudo -- cat -- /dev/null '$^' >'$@'
