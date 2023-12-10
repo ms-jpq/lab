5b752773fbb7db1045b55570926d32f00b2e7c84
IIIIII <github@bigly.dog>
2023-12-10 09:20:55 +0800

    lighthttpd

--- layers/mail/usr/local/opt/initd/makelib/mail.mk
+++ layers/mail/usr/local/opt/initd/makelib/mail.mk
@@ -11,3 +11,11 @@ mail.clobber:
 /var/lib/local/vmail: | /home/vmail
 	sudo -- mkdir --parents -- '$@'
 	sudo -- chown -- '$(@F):$(@F)' '$@'
+
+
+mail: /usr/local/opt/lighttpd/lighttpd.conf
+
+/etc/lighttpd/lighttpd.conf: | pkg._
+
+/usr/local/opt/lighttpd/lighttpd.conf: /usr/local/opt/lighttpd/libexec/cfg.sed /etc/lighttpd/lighttpd.conf
+	sudo -- /usr/local/libexec/sponge2.sh '$@' '$<' /etc/lighttpd/lighttpd.conf
