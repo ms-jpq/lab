28eb642241eb95b661e8de00bb6dabea6c0b85ee
IIIIII <github@bigly.dog>
2023-12-12 10:39:27 +0800

    apache2

--- layers/mail/usr/local/opt/initd/makelib/mail.mk
+++ layers/mail/usr/local/opt/initd/makelib/mail.mk
@@ -13,9 +13,9 @@ mail.clobber:
 	sudo -- chown -- '$(@F):$(@F)' '$@'
 
 
-mail: /usr/local/opt/lighttpd/lighttpd.conf
+mail: /usr/local/opt/apache2/apache2.conf
 
-/etc/lighttpd/lighttpd.conf: | pkg._
+/etc/apache2/apache2.conf: | pkg._
 
-/usr/local/opt/lighttpd/lighttpd.conf: /usr/local/opt/lighttpd/libexec/cfg.sed /etc/lighttpd/lighttpd.conf
-	sudo -- /usr/local/libexec/sponge2.sh '$@' '$<' /etc/lighttpd/lighttpd.conf
+/usr/local/opt/apache2/apache2.conf: /usr/local/opt/apache2/libexec/cfg.sed /etc/apache2/apache2.conf
+	sudo -- /usr/local/libexec/sponge2.sh '$@' '$<' /etc/apache2/apache2.conf
--- layers/mail/usr/local/opt/lighttpd/libexec/cfg.sed
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env -S -- sed -E -f
-
-/^server\.port.+/d
-/^server\.errorlog.+/d
-/^server\.upload-dirs/d
-/^include.+/d
-$ainclude "/usr/local/opt/lighttpd/conf.d/*.conf"
