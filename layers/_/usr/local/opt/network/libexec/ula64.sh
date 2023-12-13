2461e2738e5671dd6a8badf91c4d94bd85b05a1b
IIIIII <github@bigly.dog>
2023-12-13 13:24:15 +0800

    fix bitshift

--- layers/_/usr/local/opt/network/libexec/ula64.sh
+++ layers/_/usr/local/opt/network/libexec/ula64.sh
@@ -7,5 +7,5 @@ ULA=$((0xfd << 56))
 B2="$(b2sum --binary --length 48 <<<"$ID")"
 B2="${B2% *}"
 MASK=$((~(0xffff << 48)))
-printf -v BITS -- '%x' $(((ULA ^ "0x$B2" << 16) >> 16 & MASK))
+printf -v BITS -- '%x' $(((ULA ^ "0x$B2" << 8) >> 16 & MASK))
 exec -- perl -CASD -wpe 's/(.{4})(?=.)/$1:/g' <<<"$BITS"
