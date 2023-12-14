3227d279ca2256c2af6560d12ad90addc24a0cc0
IIIIII <github@bigly.dog>
2023-12-15 01:26:48 +0800

    fix
    unbound2

---
+++ layers/router/usr/local/opt/cgi/bin/unbound.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+if [[ ! -t 1 ]]; then
+  exec >&3
+fi
+
+tee -- <<-'EOF'
+HTTP/1.0 200 OK
+Content-Type: text/plain; charset=utf-8
+
+EOF
+
+unbound-control -c /usr/local/opt/unbound/unbound.conf stats_noreset
