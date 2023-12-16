8307840ae9dc409416e82e7f563ced30db5f42e5
IIIIII <github@bigly.dog>
2023-12-17 06:18:24 +0800

    ok3

--- layers/irc/usr/local/opt/initd/makelib/irc.mk
+++ layers/irc/usr/local/opt/initd/makelib/irc.mk
@@ -2,8 +2,10 @@
 
 all: irc
 
-irc: /etc/apt/trusted.gpg.d/weechat.gpg
+irc: /etc/apt/trusted.gpg.d/weechat.gpg /usr/local/opt/weechat/scripts
 
 /etc/apt/trusted.gpg.d/weechat.gpg:
 	$(CURL) -- 'https://weechat.org/dev/info/debian_repository_signing_key' | sudo -- gpg --batch --dearmor --yes --output '$@'
 
+/usr/local/opt/weechat/scripts:
+	git clone --depth=1 '--jobs=$(NPROC)' -- 'https://github.com/weechat/scripts.git' '$@'
---
+++ layers/irc/usr/local/opt/weechat/libexec/script.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+cd -- "${0%/*}/../scripts"
+
+OPTS='l:,n:,a:'
+LONG_OPTS='lang:,name:,action:'
+GO="$(getopt --options="$OPTS" --longoptions="$LONG_OPTS" --name="$0" -- "$@")"
+eval -- set -- "$GO"
+
+while (($#)); do
+  case "$1" in
+  --)
+    shift -- 1
+    break
+    ;;
+  -l | --lang)
+    LAN="$2"
+    shift -- 2
+    ;;
+  -n | --name)
+    NAME="$2"
+    shift -- 2
+    ;;
+  -a | --action)
+    ACTION="$2"
+    shift -- 2
+    ;;
+  *)
+    exit 1
+    ;;
+  esac
+done
+
+DATA=/var/lib/local/weechat/data
+
+SRC="$PWD/$LAN/$NAME"
+DST="$DATA/$LAN/$NAME"
+AUTO="$DATA/$LAN/autoload/$NAME"
+
+case "$ACTION" in
+install)
+  if [[ -f "$SRC" ]]; then
+    ln -v -sf -- "$SRC" "$DST"
+    ln -v -sf -- "$DST" "$AUTO"
+  fi
+  ;;
+uninstall)
+  rm -v -fr -- "$DST" "$AUTO"
+  ;;
+*)
+  exit 1
+  ;;
+esac
