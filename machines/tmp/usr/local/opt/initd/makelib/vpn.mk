668cea35965e57f16ff37ee1dfd4c6feb9e46813
IIIIII <github@bigly.dog>
2023-12-19 00:54:16 -0800

    youtube-dl

--- machines/tmp/usr/local/opt/initd/makelib/vpn.mk
+++ machines/tmp/usr/local/opt/initd/makelib/vpn.mk
@@ -1,7 +1,7 @@
 pkg._: /etc/apt/trusted.gpg.d/nordvpn.asc
 
 /etc/apt/trusted.gpg.d/nordvpn.asc:
-		$(CURL) --output '$@' -- 'https://repo.nordvpn.com/gpg/nordvpn_public.asc'
+		sudo -- $(CURL) --output '$@' -- 'https://repo.nordvpn.com/gpg/nordvpn_public.asc'
 
 show: | pkg._
 	genconf.sh
