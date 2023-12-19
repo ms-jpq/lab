ad4245c9e748218353a1608f6c758d0ee3f26074
IIIIII <github@bigly.dog>
2023-12-19 08:41:05 -0800

    w

---
+++ machines/droplet/usr/local/opt/docker-compose/libexec/network.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+NET=traefik
+
+NS="$(docker network ls --format 'json' | jq --raw-output '.Name')"
+readarray -t -- NETWORKS <<<"$NS"
+
+for NETWORK in "${NETWORKS[@]}"; do
+  if [[ "$NETWORK" == "$NET" ]]; then
+    exit
+  fi
+done
+
+exec -- docker network create --internal -- "$NET"
