398e05139a8f38ba642cb9e643201c6795d16555
IIIIII <github@bigly.dog>
2024-01-07 01:18:36 -0400

    records

--- tf/trunk/dns.tf
+++ tf/trunk/dns.tf
@@ -1,11 +1,11 @@
-resource "aws_route53_zone" "zune" {
+resource "aws_route53_zone" "sea_to_sky" {
   name = var.le_domain
 }
 
 resource "aws_route53_record" "mx" {
-  name    = "mx.${aws_route53_zone.zune.name}"
-  records = [aws_route53_zone.zune.name]
+  name    = "mx.${aws_route53_zone.sea_to_sky.name}"
+  records = ["69 ${aws_route53_zone.sea_to_sky.name}"]
   ttl     = 60
   type    = "MX"
-  zone_id = aws_route53_zone.zune.zone_id
+  zone_id = aws_route53_zone.sea_to_sky.zone_id
 }
