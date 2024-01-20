7463afb0d3c8c0ca84d95d2918bc3f1e95836894
IIIIII <github@bigly.dog>
2024-01-20 10:15:34 -0800

    ok

--- layers/_/usr/local/opt/network/libexec/ip64alloc.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
-
-set -o pipefail
-
-B2="$(b2sum --binary --length 64 -- "$@")"
-B2="${B2% *}"
-exec -- perl -CASD -wpe 's/(.{4})(?=.)/$1:/g' <<<"$B2"
