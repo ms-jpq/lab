3f099c9e897d6f8df0568260d6bb3ec82310a792
IIIIII <github@bigly.dog>
2024-01-20 19:00:25 -0800

    qemu lines 0

--- layers/qemu/usr/local/opt/cgi/bin/qemu.sh
+++ layers/qemu/usr/local/opt/cgi/bin/qemu.sh
@@ -13,5 +13,6 @@ Content-Type: text/plain; charset=utf-8
 
 EOF
 
-/usr/local/bin/microvmctl.sh || true
-/usr/local/bin/q35ctl.sh || true
+/usr/local/libexec/hr-run.sh tree --dirsfirst -F -a -L 3 -- /var/lib/local/qemu
+/usr/local/libexec/hr-run.sh systemctl --no-pager status --lines 0 -- '2-qemu-microvm@*.service' || true
+/usr/local/libexec/hr-run.sh systemctl --no-pager status --lines 0 -- '2-qemu-q35@*.service' || true
