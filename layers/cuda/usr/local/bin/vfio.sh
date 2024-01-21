a891d67ac8adeca228caea3ba51ae8e0135ef639
IIIIII <github@bigly.dog>
2024-01-21 04:28:36 -0800

    vfio2

--- layers/cuda/usr/local/bin/vfio.sh
+++ layers/cuda/usr/local/bin/vfio.sh
@@ -3,8 +3,8 @@
 set -o pipefail
 
 VFIO_IDS=(
-  '01:00.0=10de 2504'
-  '01:00.1=10de 228e'
+  01:00.0
+  01:00.1
 )
 
 MODS=(
@@ -12,6 +12,10 @@ MODS=(
   vfio_iommu_type1
   vfio_pci
 )
+
+vendor() {
+  lspci -n -m -s "$*" | awk '{ print $3 $4 }' | tr '"' ' ' | awk '{ $1=$1; print }'
+}
 
 case "$1" in
 unbind)
@@ -21,11 +25,13 @@ unbind)
 
   systemctl stop -- nvidia-persistenced.service
 
-  for VF in "${VFIO_IDS[@]}"; do
-    IOMMU="0000:${VF%=*}"
-    VENDOR="${VF#*=}"
+  for IOMMU in "${VFIO_IDS[@]}"; do
+    VENDOR="$(vendor "$IOMMU")"
+    printf -- '%q -> %q\n' "$IOMMU" "$VENDOR"
+    printf -- '%s' "$VENDOR" >"/sys/bus/pci/drivers/nvidia/remove_id" || true
     printf -- '%s' "$IOMMU" >"/sys/bus/pci/devices/$IOMMU/driver/unbind" || true
     printf -- '%s' "$VENDOR" >"/sys/bus/pci/drivers/vfio-pci/new_id" || true
+    printf -- '%s' "$IOMMU" >"/sys/bus/pci/drivers/vfio-pci/bind" || true
   done
 
   tree -- /dev/vfio
@@ -33,9 +39,9 @@ unbind)
   printf -- '\n'
   ;;
 bind)
-  for VF in "${VFIO_IDS[@]}"; do
-    IOMMU="0000:${VF%=*}"
-    VENDOR="${VF#*=}"
+  for IOMMU in "${VFIO_IDS[@]}"; do
+    VENDOR="$(vendor "$IOMMU")"
+    printf -- '%q -> %q\n' "$IOMMU" "$VENDOR"
     printf -- '%s' "$VENDOR" >"/sys/bus/pci/drivers/vfio-pci/remove_id" || true
     printf -- '%s' "$IOMMU" >"/sys/bus/pci/drivers/vfio-pci/unbind" || true
     printf -- '%s' "$VENDOR" >"/sys/bus/pci/drivers/nvidia/new_id" || true
