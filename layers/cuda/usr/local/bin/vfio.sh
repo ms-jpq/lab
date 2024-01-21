7b5d85b73d2fd7b6b87f20537e25bbf17d94a569
IIIIII <github@bigly.dog>
2024-01-21 04:11:08 -0800

    better vfio detection

--- layers/cuda/usr/local/bin/vfio.sh
+++ layers/cuda/usr/local/bin/vfio.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
 
 set -o pipefail
-set -x
 
 VFIO_IDS=(
   '01:00.0=10de 2504'
@@ -30,18 +29,23 @@ unbind)
   done
 
   tree -- /dev/vfio
+  nvidia-smi || true
+  printf -- '\n'
   ;;
 bind)
   for VF in "${VFIO_IDS[@]}"; do
     IOMMU="0000:${VF%=*}"
     VENDOR="${VF#*=}"
     printf -- '%s' "$VENDOR" >"/sys/bus/pci/drivers/vfio-pci/remove_id" || true
-    printf -- '%s' "$IOMMU" >"/sys/bus/pci/devices/$IOMMU/driver/bind" || true
+    printf -- '%s' "$IOMMU" >"/sys/bus/pci/drivers/vfio-pci/unbind" || true
+    printf -- '%s' "$VENDOR" >"/sys/bus/pci/drivers/nvidia/new_id" || true
+    printf -- '%s' "$IOMMU" >"/sys/bus/pci/drivers/nvidia/bind" || true
   done
 
   tree -- /dev/vfio
 
   systemctl start -- nvidia-persistenced.service
+  nvidia-smi
   ;;
 *)
   set -x
