c23884d17067b3598a7e33da82756e7b53f3ccbf
IIIIII <github@bigly.dog>
2024-01-24 11:09:34 -0800

    networkdhcp

--- layers/qemu/usr/local/opt/qemu/libexec/nss.sh
+++ layers/qemu/usr/local/opt/qemu/libexec/nss.sh
@@ -3,13 +3,16 @@
 set -o pipefail
 
 MACHINE="$1"
-HOSTS="$2"
-DOMAIN="$3"
+DNSMAQ_CONF="$2"
+HOSTS="$3"
+DOMAIN="$4"
 
 LO64="$(/usr/local/opt/network/libexec/ip64alloc.sh <<<"$MACHINE")"
 # shellcheck disable=2154
 IPV6="$IPV6_NETWORK:$LO64"
 
+cp -v -- "${0%/*}/../dhcp.conf" "$DNSMAQ_CONF/dhcp.conf"
+
 sponge -- "$HOSTS/qemu" <<-EOF
-$IPV6 _qemu.$DOMAIN $MACHINE.$DOMAIN
+$IPV6 _qemu.$DOMAIN
 EOF
