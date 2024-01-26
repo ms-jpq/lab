ea5da7b1ad5ecc1d3288145373738dd3a7d0c31e
IIIIII <github@bigly.dog>
2024-01-26 00:19:15 -0800

    ordering of m4

--- layers/router/usr/local/lib/systemd/system-generators/lan-ifs.sh
+++ layers/router/usr/local/lib/systemd/system-generators/lan-ifs.sh
@@ -7,15 +7,22 @@ WANTS="$RUN/multi-user.target.wants"
 
 # shellcheck disable=SC1091
 source -- /usr/local/etc/default/lan.env
-
 # shellcheck disable=SC2154
-readarray -t -d ' ' -- INTERFACES <<<"$LAN_IFS"
+LAN_IFS="$(sort <<<"${LAN_IFS//' '/$'\n'}")"
+
+readarray -t -- INTERFACES <<<"$LAN_IFS"
+SEEN=()
 
 mkdir -v -p -- "$WANTS"
 for NAME in "${INTERFACES[@]}"; do
   NAME="$(systemd-escape -- "${NAME//[[:space:]]/''}")"
   ALLOCD="$RUN/1-ip-alloc@$NAME.service.d"
   mkdir -v -p -- "$ALLOCD"
-  cp -v -- /usr/local/opt/network/1-ip-alloc@.service "$ALLOCD/0-override.conf"
+
+  IFS=','
+  PREV="ENV_IFS=${SEEN[*]}"
+  unset -- IFS
+  /usr/local/libexec/m4.sh -D"$PREV" -- /usr/local/opt/network/1-ip-alloc@.service.m4 >"$ALLOCD/0-override.conf"
   ln -v -sf -- /usr/local/lib/systemd/system/1-lan@.service "$WANTS/1-lan@$NAME.service"
+  SEEN+=("$NAME")
 done
---
+++ layers/router/usr/local/opt/network/1-ip-alloc@.service.m4
@@ -0,0 +1,6 @@
+[[Unit]]
+After = 1-ip-alloc@wg0.service
+
+# m5_for([IF_NAME], [
+After = 1-ip-alloc@IF_NAME.service
+# ], m5_assert([ENV_IFS]))
