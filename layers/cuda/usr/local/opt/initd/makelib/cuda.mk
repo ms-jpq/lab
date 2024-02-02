1979b0a2b7cd9d07c57ba64e04a9cacaba54025e
IIIIII <github@bigly.dog>
2024-02-02 15:13:13 -0800

    duda2

--- layers/cuda/usr/local/opt/initd/makelib/cuda.mk
+++ layers/cuda/usr/local/opt/initd/makelib/cuda.mk
@@ -3,7 +3,11 @@
 # /etc/apt/sources.list.d/ppa_graphics-drivers_ppa.list:
 # 	sudo -- ./libexec/add-ppa.sh graphics-drivers/ppa
 
-pkg._: /etc/apt/trusted.gpg.d/cuda.gpg
-/etc/apt/trusted.gpg.d/cuda.gpg: | /usr/local/opt/cuda/libexec/gpg.sh
+CUDA_GPG := /etc/apt/trusted.gpg.d/cuda.gpg
+
+pkg._: $(CUDA_GPG)
+$(CUDA_GPG): | /usr/local/opt/cuda/libexec/gpg.sh
 	LINK="$$('$|')"
 	$(CURL) -- "$$LINK" >'$@'
+
+$(CACHE)/nspawn/cloud.img: | $(CUDA_GPG)
