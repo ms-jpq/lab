794a5581a123adb75781aeac45b6fd4452014af9
IIIIII <github@bigly.dog>
2024-02-07 10:35:53 -0800

    w

--- terraform/wtc2/srv.tf
+++ terraform/wtc2/srv.tf
@@ -40,8 +40,9 @@ resource "aws_lightsail_instance_public_ports" "acab" {
 
 resource "aws_lightsail_disk_attachment" "john_cena" {
   provider      = aws.us_w2
-  disk_name     = "iscsi-droplet"
-  disk_path     = "/dev/xvdp"
+  for_each      = { "iscsi-droplet" = "/dev/xvdp" }
+  disk_name     = each.key
+  disk_path     = each.value
   instance_name = aws_lightsail_instance.droplet.name
 }
 
