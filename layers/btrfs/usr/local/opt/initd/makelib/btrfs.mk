b6b2bf9fb24e7cec123b75691480d410f505939a
IIIIII <github@bigly.dog>
2024-02-10 16:49:58 -0800

    main

--- layers/btrfs/usr/local/opt/initd/makelib/btrfs.mk
+++ layers/btrfs/usr/local/opt/initd/makelib/btrfs.mk
@@ -1,2 +1,6 @@
-/usr/local/opt/btrfs/main.conf: $(shell shopt -u failglob && printf -- '%s ' /usr/local/opt/btrbk/conf.d/*)
+.PHONY: btrfs
+all: btrfs
+
+btrfs: /usr/local/opt/btrbk/main.conf
+/usr/local/opt/btrbk/main.conf: $(shell shopt -u failglob && printf -- '%s ' /usr/local/opt/btrbk/conf.d/*.conf)
 	cat -- /dev/null $^ | sudo -- tee -- '$@'
