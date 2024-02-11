e77d164ea4cae9d96bd108c29686775649138792
IIIIII <github@bigly.dog>
2024-02-11 00:57:21 -0800

    zdiff

---
+++ layers/zfs/usr/local/bin/zfdiff.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+case $# in
+1)
+  LINES="$(zfs list -t snapshot -o name -H -- "$@" | tail --lines 2)"
+  readarray -t -- LR <<<"$LINES"
+  LHS="${LR[0]}"
+  RHS="${LR[1]}"
+  ;;
+2)
+  NAME="$1"
+  shift -- 1
+  LHS="$NAME$*"
+  RHS="$(zfs list -t snapshot -o name -H -- "$NAME" | tail --lines 1)"
+  ;;
+3)
+  NAME="$1"
+  shift -- 1
+  LHS="$NAME$1"
+  RHS="$NAME$2"
+  ;;
+*)
+  set -x
+  exit 1
+  ;;
+esac
+
+exec -- /usr/local/libexec/hr-run.sh zfs diff -F -- "$LHS" "$RHS"
