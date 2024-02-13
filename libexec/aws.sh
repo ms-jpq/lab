fe45afa0f62665623d7268e6ee2201b77b1f5e1e
IIIIII <github@bigly.dog>
2024-02-13 07:29:18 -0800

    k

--- libexec/aws.sh
+++ libexec/aws.sh
@@ -52,7 +52,7 @@ cpu)
   IS="$("${AWS[@]}" get-instances | "${JQ[@]}" '.instances[].name')"
   readarray -t -- INSTANCES <<<"$IS"
   read -r -d '' -- JQJQ <<-'EOF' || true
-.metricData[] | "\(.timestamp | sub("T"; " ") | sub("-\\d\\d:\\d\\d$"; "")) -> \(.average | floor) \(.unit)"
+.metricData[] | "\(.timestamp | sub("T"; " ") | sub("-..:..$"; "")) -> \(.average | floor) \(.unit)"
 EOF
 
   for INSTANCE in "${INSTANCES[@]}"; do
