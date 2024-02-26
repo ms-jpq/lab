bd3e4660b360ca228d2fff33b26ef4f5852f2cd5
IIIIII <github@bigly.dog>
2024-02-26 01:05:58 -0800

    w

--- layers/btrfs/usr/local/opt/btrfs/libexec/defrag.sh
+++ layers/btrfs/usr/local/opt/btrfs/libexec/defrag.sh
@@ -4,13 +4,13 @@ set -o pipefail
 
 SUBVOLUME="$1"
 LIST=(btrfs subvolume list -- "$SUBVOLUME")
-CUT=(cut --delimiter ' ' --fields 9-)
+PICK=(cut --delimiter ' ' --fields 9-)
 PARALLEL=(xargs --no-run-if-empty --null -I % --max-procs 0 --)
 DEFRAG=(btrfs -v filesystem defragment -r -- %)
 
 {
   printf -- '%s\0' "$SUBVOLUME"
-  "${LIST[@]}" | "${CUT[@]}" | while read -r -- VOL; do
+  "${LIST[@]}" | "${PICK[@]}" | while read -r -- VOL; do
     VOL="$SUBVOLUME/$VOL"
     if btrfs property get -- "$VOL" ro | grep -F -- 'ro=false' >/dev/null; then
       printf -- '%s\0' "$VOL"
