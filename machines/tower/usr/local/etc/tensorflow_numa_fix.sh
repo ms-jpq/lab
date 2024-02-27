34d5e05807bb6c1c1b77dcd8557c505ab286b98b
IIIIII <github@bigly.dog>
2024-02-27 14:38:54 -0800

    nft

---
+++ machines/tower/usr/local/etc/tensorflow_numa_fix.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+PCI="$(lspci -mm | awk '/VGA/ && /NVIDIA/ { print "0000:"$1 }')"
+
+NODE="/sys/bus/pci/devices/$PCI/numa_node"
+printf -- '%s' 1 >"$NODE"
