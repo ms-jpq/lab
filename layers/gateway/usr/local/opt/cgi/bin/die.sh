0576a7cf57605d76821326e72b244da34c756811
IIIIII <github@bigly.dog>
2024-02-28 12:49:23 -0800

    logout

--- layers/gateway/usr/local/opt/cgi/bin/die.sh
+++ layers/gateway/usr/local/opt/cgi/bin/die.sh
@@ -3,12 +3,30 @@
 set -o pipefail
 
 if [[ ! -t 1 ]]; then
-  exec >&3
+  exec <&3 >&3
 fi
 
-tee -- <<-'EOF'
+DOMAIN=''
+while read -r LINE; do
+  LINE="${LINE%$'\r'}"
+  if [[ -z "$LINE" ]]; then
+    break
+  fi
+
+  LHS="${LINE%%:*}"
+  KEY="${LHS,,}"
+  case "$KEY" in
+  host)
+    DOMAIN="$(sed -E -e 's/.*\.([^.]+\.[^.]+)$/\1/' <<<"${LINE##*: }")"
+    DOMAIN="Domain=$DOMAIN"
+    ;;
+  *) ;;
+  esac
+done
+
+tee -- <<-EOF
 HTTP/1.0 307
+Set-Cookie: _htpasswd=; Max-Age=0; HttpOnly; Path=/; $DOMAIN
 Location: /
-Clear-Site-Data: "*"
 
 EOF
