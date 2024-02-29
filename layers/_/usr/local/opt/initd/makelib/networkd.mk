a2cb17587bd4138268b46ff4868dfb4ff5772915
IIIIII <github@bigly.dog>
2024-02-28 22:33:02 -0800

    fix

--- layers/_/usr/local/opt/initd/makelib/networkd.mk
+++ layers/_/usr/local/opt/initd/makelib/networkd.mk
@@ -1,7 +1,14 @@
-all: /usr/local/lib/systemd/network/._touch
 
 /usr/lib/systemd/network: pkg._
 
-/usr/local/lib/systemd/network/._touch: $(shell shopt -u failglob && printf -- '%s ' /usr/local/lib/systemd/network/**/!(._touch)) | /usr/lib/systemd/network
-	sudo -- chown -v -R -- systemd-network:systemd-network '$(@D)'
-	sudo -- touch -- '$@'
+define SYSTEMD_CHOWN_TEMPLATE
+all: $1/._touch
+$1/._touch: $(shell shopt -u failglob && printf -- '%s ' $1/**/!(._touch)) | /usr/lib/systemd/network
+	sudo -- chown -v -R -- $2:$2 '$$(@D)'
+	sudo -- touch -- '$$@'
+endef
+
+$(eval $(call SYSTEMD_CHOWN_TEMPLATE,/usr/local/lib/systemd/network,systemd-network))
+$(eval $(call SYSTEMD_CHOWN_TEMPLATE,/usr/local/lib/systemd/networkd.conf.d,systemd-network))
+$(eval $(call SYSTEMD_CHOWN_TEMPLATE,/usr/local/lib/systemd/dnssd,systemd-resolve))
+$(eval $(call SYSTEMD_CHOWN_TEMPLATE,/usr/local/lib/systemd/resolved.conf.d,systemd-resolve))
