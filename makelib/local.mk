8344916cd8e3e0693ae065e122afc9a0c1ccf819
IIIIII <github@bigly.dog>
2024-02-29 15:07:48 -0800

    o+r

--- makelib/local.mk
+++ makelib/local.mk
@@ -94,6 +94,7 @@ local: $(TMP)/$1/fs
 
 $(TMP)/$1/fs: $(VAR)/sh/libexec/lsync.sh $$(LOCALS.$1) | $(VAR)/sh
 	'$$<' '$$@' '$(TMP)/$1/layers'/*/
+	find "$$@/usr/local/lib/systemd" -type f -print0 | xargs -r -0 -- chmod o+r
 endef
 
 
