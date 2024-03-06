e4427b2a98699160d9e59cb58f95e207507640fb
IIIIII <github@bigly.dog>
2024-03-05 21:59:38 -0800

    pkg

--- layers/_/usr/local/opt/initd/makelib/pkg.mk
+++ layers/_/usr/local/opt/initd/makelib/pkg.mk
@@ -4,7 +4,7 @@ pkg: pkg._
 pkg._: ./libexec/pkg.sh
 	'$<'
 
-/usr/bin/unzip /usr/bin/envsubst /usr/bin/batcat /usr/bin/fdfind: | pkg._
+/usr/bin/unzip /usr/bin/envsubst /usr/bin/batcat /usr/bin/fdfind /usr/lib/systemd/network: | pkg._
 pkg: | /usr/local/bin/bat /usr/local/bin/fd
 
 /usr/local/bin/bat: | /usr/bin/batcat
