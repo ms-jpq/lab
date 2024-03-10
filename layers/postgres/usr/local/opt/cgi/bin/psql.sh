a0aa19472daf6d1e3fe7439a88b2c0676343c79c
IIIIII <github@bigly.dog>
2024-03-09 17:27:35 -0800

    upread

--- layers/postgres/usr/local/opt/cgi/bin/psql.sh
+++ layers/postgres/usr/local/opt/cgi/bin/psql.sh
@@ -1,13 +1,44 @@
-#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O globstar
 
 set -o pipefail
 
 if [[ ! -t 1 ]]; then
-  exec >&3
+  exec <&3 >&3
 fi
+
+HOST="$HOSTNAME"
+while read -r LINE; do
+  LINE="${LINE%$'\r'}"
+  if [[ -z "$LINE" ]]; then
+    break
+  fi
+
+  LHS="${LINE%%:*}"
+  KEY="${LHS,,}"
+  case "$KEY" in
+  host)
+    HOST="${LINE##*: }"
+    ;;
+  *) ;;
+  esac
+done
 
 tee -- <<-'EOF'
 HTTP/1.0 200 OK
 Content-Type: text/plain; charset=utf-8
 
 EOF
+
+for F in /var/lib/local/postgresql/*/init.user; do
+  NAME="${F%/*}"
+  NAME="${NAME##*/}"
+  printf -- '%s\n' "$NAME"
+  LINE="$(<"$F")"
+  printf -- '%s\n' "$LINE"
+  NAME="${LINE%%' '*}"
+  PASS="$(cut --delimiter ' ' --fields 3- <<<"$LINE" | jq --exit-status --raw-input --raw-output '@uri')"
+  printf -v PSQL -- '%q ' psql -- "postgres://$NAME:$PASS@$HOST/$NAME"
+  printf -- '%s\n' "$PSQL"
+  /usr/local/libexec/hr.sh
+  printf -- '\n'
+done
