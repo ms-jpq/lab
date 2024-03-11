59406670ff55d67c2226967fd4aa6b7acf897ff9
IIIIII <github@bigly.dog>
2024-03-11 14:49:06 -0700

    openvpn

---
+++ layers/openvpn/usr/local/opt/cgi/bin/ovpn.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+if [[ ! -t 1 ]]; then
+  exec >&3
+fi
+
+tee -- <<-'EOF'
+HTTP/1.0 200 OK
+Content-Type: text/plain; charset=utf-8
+
+EOF
+
+exec -- /usr/local/libexec/hr-run.sh tree -- /var/tmp
