a1efa7ef6bd587aa545a0810b637e2588b85254a
IIIIII <github@bigly.dog>
2024-03-12 12:41:57 -0700

    no microsoft

--- layers/tv/usr/local/opt/initd/makelib/tv.mk
+++ layers/tv/usr/local/opt/initd/makelib/tv.mk
@@ -2,7 +2,7 @@ pkg._: /etc/apt/sources.list.d/ppa_mozillateam_ppa.list
 /etc/apt/sources.list.d/ppa_mozillateam_ppa.list:
 	sudo -- ./libexec/add-ppa.sh mozillateam/ppa
 
-pkg._: /etc/apt/trusted.gpg.d/microsoft.gpg
-/etc/apt/trusted.gpg.d/microsoft.gpg:
-	$(CURL) -- 'https://packages.microsoft.com/keys/microsoft.asc' | sudo -- gpg --batch --dearmor --yes --output '$@'
+# pkg._: /etc/apt/trusted.gpg.d/microsoft.gpg
+# /etc/apt/trusted.gpg.d/microsoft.gpg:
+# 	$(CURL) -- 'https://packages.microsoft.com/keys/microsoft.asc' | sudo -- gpg --batch --dearmor --yes --output '$@'
 
