ced675b5b035cec25c14e8cb3f8e915ef87c506b
IIIIII <github@bigly.dog>
2024-03-19 20:37:32 -0700

    Revert "k"
    This reverts commit 9c18016ff711f101f2aeacb1b14b59f23bc374fe.

--- layers/mail/usr/local/opt/initd/makelib/mail.mk
+++ layers/mail/usr/local/opt/initd/makelib/mail.mk
@@ -7,6 +7,13 @@ mail: /var/lib/local/vmail
 	sudo -- mkdir -v --parents -- '$@'
 	sudo -- chown -v -- '1000:1000' '$@'
 
+mail: /usr/local/opt/apache2/apache2.conf
+
+/etc/apache2/apache2.conf: | pkg._
+
+/usr/local/opt/apache2/apache2.conf: /usr/local/opt/apache2/libexec/cfg.sed /etc/apache2/apache2.conf
+	sudo -- /usr/local/libexec/sponge2.sh '$@' '$<' /etc/apache2/apache2.conf
+
 
 define ZPUSH_TEMPLATE
 /opt/z-push/$1/config.php: | pkg._
@@ -14,7 +21,7 @@ define ZPUSH_TEMPLATE
 mail: /usr/local/opt/z-push/$(patsubst .%,%,$2.conf.php)
 /usr/local/opt/z-push/$(patsubst .%,%,$2.conf.php): /usr/local/opt/z-push/libexec/$2.sed /opt/z-push/$1/config.php
 	sudo -- /usr/local/libexec/sponge2.sh '$$@' '$$<' /opt/z-push/$1/config.php
-	! git diff --no-index --no-prefix --color-moved -- '/opt/z-push/$1/config.php' '$$@' | delta --side-by-side --light --width=120
+	! git diff --no-index --no-prefix --color-moved -- '/opt/z-push/$1/config.php' '$$@'
 endef
 
 Z_PUSH_PHP := autodiscover backend/imap .
