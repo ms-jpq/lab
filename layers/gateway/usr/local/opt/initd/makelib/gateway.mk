9194435bbe139858c2fd43942cf51175eda1ee11
IIIIII <github@bigly.dog>
2024-03-19 17:05:49 -0700

    k

--- layers/gateway/usr/local/opt/initd/makelib/gateway.mk
+++
@@ -1,6 +0,0 @@
-.PHONY: gateway
-all: gateway
-
-gateway: $(CACHE)/certbot/venv/bin/certbot
-$(CACHE)/certbot/venv/bin/certbot:
-	sudo -- /usr/local/opt/certbot/libexec/ensurepip.sh $(CACHE)/certbot
