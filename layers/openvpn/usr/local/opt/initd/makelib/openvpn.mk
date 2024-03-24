3f35945cc381e95306dc9fff1cf72167b2c9f6f6
IIIIII <github@bigly.dog>
2024-03-23 22:02:20 -0700

    ovpn settings

--- layers/openvpn/usr/local/opt/initd/makelib/openvpn.mk
+++ layers/openvpn/usr/local/opt/initd/makelib/openvpn.mk
@@ -2,6 +2,6 @@
 all: openvpn
 
 /usr/lib/systemd/system/openvpn@.service: | pkg._
-openvpn: /usr/local/lib/systemd/system/1-ovpn-tcp-server@.service /usr/local/lib/systemd/system/1-ovpn-udp@.service
-/usr/local/lib/systemd/system/1-ovpn-tcp-server@.service /usr/local/lib/systemd/system/1-ovpn-udp@.service: /usr/lib/systemd/system/openvpn@.service
+openvpn: /usr/local/lib/systemd/system/1-ovpn-udp@.service /usr/local/lib/systemd/system/1-ovpn-tcp\x2dserver@.service
+/usr/local/lib/systemd/system/1-ovpn-udp@.service /usr/local/lib/systemd/system/1-ovpn-tcp\x2dserver@.service: /usr/lib/systemd/system/openvpn@.service
 	sudo -- cp -v -f -- '$<' '$@'
