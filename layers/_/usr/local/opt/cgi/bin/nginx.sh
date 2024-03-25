7b18061f48b5b609971d8fbd026fc0fc9d854c60
IIIIII <github@bigly.dog>
2024-03-25 11:48:43 -0700

    80

--- layers/_/usr/local/opt/cgi/bin/nginx.sh
+++ layers/_/usr/local/opt/cgi/bin/nginx.sh
@@ -12,5 +12,8 @@ Content-Type: text/plain; charset=utf-8
 
 EOF
 
+GIXY='/opt/python3/gixy'
+
 /usr/local/libexec/hr-run.sh curl --fail-with-body --location --unix /run/local/nginx/cgi.sock --no-progress-meter -- localhost/nginx
 /usr/local/libexec/hr-run.sh nginx -c /usr/local/opt/nginx/conf/main.nginx -T
+PYTHONPATH="$GIXY" /usr/local/libexec/hr-run.sh "$GIXY/bin/gixy" -- /usr/local/opt/nginx/conf/main.nginx
