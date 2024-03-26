9b24918e19388fda839ab0f096f874b4d88885da
IIIIII <github@bigly.dog>
2024-03-25 21:36:29 -0700

    btrfs

---
+++ layers/_/usr/local/lib/systemd/system-generators/nfs.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O globstar
+
+set -o pipefail
+
+RUN="$1"
+WANTS="$RUN/remote-fs.target.wants"
+
+# shellcheck disable=SC1091
+source -- /usr/local/etc/default/nfs.env
+
+# shellcheck disable=SC2154
+readarray -t -d ',' -- MOUNTS <<<"$NFS_MNTS"
+
+mkdir -v -p -- "$WANTS"
+for MOUNT in "${MOUNTS[@]}"; do
+  MOUNT="${MOUNT//[[:space:]]/''}"
+  NFS_SERVER="${MOUNT%%':'*}"
+  DIR="$(systemd-escape -- "${MOUNT#*':/'}")"
+  MNT="$RUN/$DIR.mount"
+  AUTO="$DIR.automount"
+  NFS_SERVER="$NFS_SERVER" envsubst </usr/local/opt/mount/nfs@.mount >"$MNT"
+  cp -v -f -- /usr/local/opt/mount/@.automount "$RUN/$AUTO"
+  chmod g+r,o+r -- "$MNT" "$RUN/$AUTO"
+  ln -v -sf -- "../$AUTO" "$WANTS/$AUTO"
+done
