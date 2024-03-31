91c3fa5cacdd20e599d509b3a17d4061221e6923
IIIIII <github@bigly.dog>
2024-03-30 20:38:54 -0700

    tc

--- layers/router/usr/local/opt/cgi/bin/tc.sh
+++ layers/router/usr/local/opt/cgi/bin/tc.sh
@@ -12,11 +12,16 @@ Content-Type: text/plain; charset=utf-8
 
 EOF
 
+TMP=$(mktemp)
+
 read -r -d '' -- AWK <<-'AWK' || true
 BEGIN { command = "stdbuf --output L -- numfmt --to si --field 2-" }
 
 {
-  if (/^\s+\w+(\s+[0-9]+)+$/) {
+  if (NR == 1) {
+    print ">" $0
+  }
+  else if (/^\s+\w+(\s+[0-9]+)+$/) {
     print |& command
     command |& getline line
     print line
@@ -27,12 +32,41 @@ BEGIN { command = "stdbuf --output L -- numfmt --to si --field 2-" }
 END { close(command) }
 AWK
 
+SED=(
+  sed -E
+  -e '1s/[[:space:]]{2,}/%/g'
+  -e '1s/[[:space:]]+/_/g'
+  -e '1s/%/ /g'
+)
+
 show() {
-  /usr/local/libexec/hr-run.sh tc -statistics qdisc show dev "$2"
+  /usr/local/libexec/hr-run.sh tc -statistics qdisc show dev "$2" >"$TMP"
+  readarray -t -- LINES <"$TMP"
+
+  EMPTY=0
+  M1=-1
+  M2=-1
+  for IDX in "${!LINES[@]}"; do
+    if [[ "${LINES["$IDX"]}" =~ ^[[:space:]]*$ ]]; then
+      case $((++EMPTY)) in
+      1)
+        M1=$((IDX + 1))
+        ;;
+      2)
+        M2=$((IDX))
+        ;;
+      *) ;;
+      esac
+    fi
+  done
+
+  {
+    printf -- '%s\n' "${LINES[@]:0:M1}"
+    printf -- '%s\n' "${LINES[@]:M1:M2-M1}" | awk "$AWK" | "${SED[@]}" | column -t | pr --omit-pagination --indent=2
+    printf -- '%s\n' "${LINES[@]:M2}"
+  }
 }
 
-{
-  show RX cake-rx
-  # shellcheck disable=SC2154
-  show TX "$WAN_IF"
-} | awk "$AWK"
+show RX cake-rx
+# shellcheck disable=SC2154
+show TX "$WAN_IF"
