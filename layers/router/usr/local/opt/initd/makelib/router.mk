19fde5960a0bb05830f2c3945bbe04e82ba7e052
IIIIII <github@bigly.dog>
2024-04-30 12:39:49 -0700

    rm squid

--- layers/router/usr/local/opt/initd/makelib/router.mk
+++ layers/router/usr/local/opt/initd/makelib/router.mk
@@ -1,10 +1,1 @@
 CLOBBER.FS += /etc/jool/*
-.PHONY: squid
-all: squid
-
-/etc/squid-deb-proxy/squid-deb-proxy.conf: pkg._
-
-squid: /usr/local/opt/squid/etc/squid.conf
-/usr/local/opt/squid/etc/squid.conf: /usr/local/opt/squid/squid.conf /etc/squid-deb-proxy/squid-deb-proxy.conf
-	sudo -- cp -v -f -- '$<' '$@'
-	grep -E -- '^refresh_pattern' '/etc/squid-deb-proxy/squid-deb-proxy.conf' | sudo -- tee --append -- '$@'
