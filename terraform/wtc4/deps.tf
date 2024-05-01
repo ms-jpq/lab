5a9865878b71c76ea32bcf1f89909dea02b42ba1
IIIIII <github@bigly.dog>
2024-05-01 02:56:39 -0700

    added google

--- terraform/wtc4/deps.tf
+++ terraform/wtc4/deps.tf
@@ -1,20 +1,12 @@
 terraform {
   required_version = ">= 0"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 0"
-    }
-    external = {
-      source  = "hashicorp/external"
+    http = {
+      source  = "hashicorp/http"
       version = ">= 0"
     }
     google = {
       source  = "hashicorp/google"
-      version = ">= 0"
-    }
-    vultr = {
-      source  = "vultr/vultr"
       version = ">= 0"
     }
   }
@@ -25,7 +17,7 @@ terraform {
     bucket         = "kfc-tfstate"
     dynamodb_table = "tfstate"
     encrypt        = true
-    key            = "wsb.json"
+    key            = "wtc4.json"
     region         = "ca-west-1"
     # TODO: next release should fix this
     skip_region_validation = true
