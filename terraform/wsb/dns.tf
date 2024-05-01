a9ca41eafb6a42db9031bf160bdfdede98ad1208
IIIIII <github@bigly.dog>
2024-05-01 10:22:30 -0700

    gcp

--- terraform/wsb/dns.tf
+++ terraform/wsb/dns.tf
@@ -19,8 +19,8 @@ resource "aws_lightsail_domain" "sea_to_sky" {
 resource "google_dns_managed_zone" "sea_to_sky" {
   provider = google.ca_e2
   for_each = toset([var.le_domain])
-  name     = "s2s-${each.key}"
-  dns_name = each.key
+  name     = replace(each.key, ".", "-")
+  dns_name = "${each.key}."
 }
 
 output "google_dns_name_servers" {
--- terraform/wsb/gcp.tf
+++ terraform/wsb/gcp.tf
@@ -1,4 +1,6 @@
 provider "google" {
-  alias  = "ca_e2"
-  region = "northamerica-northeast2"
+  alias       = "ca_e2"
+  credentials = "${path.module}/../../facts/gcp.env.json"
+  project     = "unit0-257718"
+  region      = "northamerica-northeast2"
 }
