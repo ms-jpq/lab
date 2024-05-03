69d32d6c651feabefc11251ed4e1342a747bd237
IIIIII <github@bigly.dog>
2024-05-03 12:49:25 -0700

    ns2

--- terraform/bootstrap/gcp_ns.tf
+++ terraform/bootstrap/gcp_ns.tf
@@ -23,6 +23,22 @@ resource "google_service_account" "wc3" {
   project    = each.value.project_id
 }
 
+data "google_iam_role" "kleptocrat" {
+  name = "roles/owner"
+}
+
+resource "google_project_iam_member" "wc3" {
+  for_each = google_service_account.wc3
+  project  = each.value.project
+  role     = data.google_iam_role.kleptocrat.name
+  member   = "serviceAccount:${each.value.email}"
+}
+
+resource "google_service_account_key" "wc3" {
+  for_each           = google_service_account.wc3
+  service_account_id = each.value.id
+}
+
 output "gcp_projects" {
   value = { for _, acc in google_service_account.wc3 : acc.project => acc.email }
 }
