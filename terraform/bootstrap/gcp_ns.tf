ebe9753ed7ad12d004ef514edbecdb4c9d08de64
IIIIII <github@bigly.dog>
2024-05-03 13:39:51 -0700

    ns2

--- terraform/bootstrap/gcp_ns.tf
+++ terraform/bootstrap/gcp_ns.tf
@@ -2,6 +2,10 @@ provider "google" {
   alias = "root"
 }
 
+data "google_billing_account" "tinker" {
+  display_name = "tinker"
+}
+
 locals {
   gcp_projects = {
     dalaran   = "dalaran-3",
@@ -11,16 +15,21 @@ locals {
 }
 
 resource "google_project" "wc3" {
-  provider   = google.root
-  for_each   = local.gcp_projects
-  name       = each.key
-  project_id = each.value
+  for_each        = local.gcp_projects
+  billing_account = data.google_billing_account.tinker.id
+  name            = each.key
+  project_id      = each.value
 }
 
-resource "google_service_account" "wc3" {
+resource "google_service_account" "cenarius" {
   for_each   = google_project.wc3
   account_id = "sudo-user"
   project    = each.value.project_id
+}
+
+data "google_iam_role" "oligarch" {
+  for_each = toset(["roles/billing.admin"])
+  name     = each.key
 }
 
 data "google_iam_role" "kleptocrat" {
@@ -28,29 +37,51 @@ data "google_iam_role" "kleptocrat" {
   name     = each.key
 }
 
-resource "google_project_iam_member" "wc3" {
-  for_each = merge([for k1, acc in google_service_account.wc3 : {
-    for k2, role in data.google_iam_role.kleptocrat :
-    "${k1}-${k2}" => {
-      email   = acc.email
-      project = acc.project
-      role    = role.name
-    }
+resource "google_billing_account_iam_member" "tinker" {
+  for_each = merge([
+    for k1, acc in google_service_account.cenarius : {
+      for k2, role in data.google_iam_role.oligarch :
+      "${k1}-${k2}" => {
+        email   = acc.email
+        project = k1
+        role    = role.name
+      }
+  }]...)
+  billing_account_id = google_project.wc3[each.value.project].billing_account
+  member             = "serviceAccount:${each.value.email}"
+  role               = each.value.role
+}
+
+resource "google_project_iam_member" "druid" {
+  for_each = merge([
+    for k1, acc in google_service_account.cenarius : {
+      for k2, role in data.google_iam_role.kleptocrat :
+      "${k1}-${k2}" => {
+        email   = acc.email
+        project = acc.project
+        role    = role.name
+      }
   }]...)
   member  = "serviceAccount:${each.value.email}"
   project = each.value.project
   role    = each.value.role
 }
 
-resource "google_service_account_key" "wc3" {
-  for_each           = google_service_account.wc3
+resource "google_service_account_key" "fel" {
+  for_each           = google_service_account.cenarius
   service_account_id = each.value.id
 }
 
-output "gcp_projects" {
-  value = { for _, acc in google_service_account.wc3 :
-    acc.project => {
-      owner = acc.email
+output "gcp" {
+  value = {
+    billing = {
+      id = data.google_billing_account.tinker.id
+    }
+    projects = {
+      for _, acc in google_service_account.cenarius :
+      acc.project => {
+        owner = acc.email
+      }
     }
   }
 }
