32fab555eb5ad00f9bd79e0a01899074509895ab
IIIIII <github@bigly.dog>
2024-05-03 16:12:50 -0700

    wow

--- terraform/wsb/budget_gcp.tf
+++ terraform/wsb/budget_gcp.tf
@@ -16,35 +16,34 @@ data "google_iam_role" "pmc" {
   name = "roles/serviceusage.serviceUsageConsumer"
 }
 
-data "google_service_account" "dalaran" {
-  provider   = google.dalaran
+data "google_service_account" "kalimdor" {
+  provider   = google.kalimdor
   account_id = "sudo-user"
 }
 
 resource "google_project_iam_member" "kalimdor_pmc" {
-  provider = google.kalimdor
-  member   = "serviceAccount:${data.google_service_account.dalaran.email}"
-  project  = "kalimdor-3"
+  provider = google.dalaran
+  member   = "serviceAccount:${data.google_service_account.kalimdor.email}"
+  project  = "dalaran-3"
   role     = data.google_iam_role.pmc.name
 }
 
-# resource "google_billing_budget" "septims" {
-#   provider        = google.kalimdor
-#   billing_account = data.google_billing_account.tinker.id
-#   display_name    = "Example Billing Budget"
-#   amount {
-#     specified_amount {
-#       currency_code = "USD"
-#       units         = tostring(local.budget)
-#     }
-#   }
-#   dynamic "threshold_rules" {
-#     for_each = toset([
-#       for i in range(0, 1) :
-#       tostring(1 + i * 0.05)
-#     ])
-#     content {
-#       threshold_percent = tonumber(threshold_rules.key)
-#     }
-#   }
-# }
+resource "google_billing_budget" "septims" {
+  provider        = google.kalimdor
+  billing_account = data.google_billing_account.tinker.id
+  amount {
+    specified_amount {
+      currency_code = "USD"
+      units         = tostring(local.budget)
+    }
+  }
+  dynamic "threshold_rules" {
+    for_each = toset([
+      for i in range(0, 1) :
+      tostring(1 + i * 0.05)
+    ])
+    content {
+      threshold_percent = tonumber(threshold_rules.key)
+    }
+  }
+}
