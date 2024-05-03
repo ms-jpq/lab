7a43d64e7a3baf4e12c76916fedd5b4221044360
IIIIII <github@bigly.dog>
2024-05-02 23:03:20 -0700

    http2 same port

--- layers/_/usr/local/opt/initd/makelib/nginx.mk
+++ layers/_/usr/local/opt/initd/makelib/nginx.mk
@@ -1,6 +1,10 @@
 .PHONY: nginx nginx.lint
 all: nginx
 
+pkg._: /etc/apt/trusted.gpg.d/nginx.gpg
+/etc/apt/trusted.gpg.d/nginx.gpg:
+	$(CURL) -- 'https://nginx.org/keys/nginx_signing.key' | sudo -- gpg --batch --dearmor --yes --output '$@'
+
 nginx: /usr/local/opt/nginx/conf/._touch
 /usr/local/opt/nginx/conf/._touch: $(shell shopt -u failglob && printf -- '%s ' /usr/local/opt/nginx/conf/**/*.nginx)
 	sudo -- /usr/local/libexec/try-reload.sh nginx.service
