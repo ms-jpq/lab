d03db286b17c0f1086ec3cca0d63bb368f6f0aa5
IIIIII <github@bigly.dog>
2024-05-03 09:29:58 -0700

    b2

--- terraform/wsb/budget.tf
+++ terraform/wsb/budget.tf
@@ -54,22 +54,39 @@ resource "google_billing_account_iam_member" "tinker" {
   role               = data.google_iam_role.bill_gates.name
 }
 
-# resource "google_billing_budget" "septims" {
-#   provider        = google.ca_e2
-#   billing_account = var.gcp_billing_account
-#   amount {
-#     specified_amount {
-#       currency_code = "USD"
-#       units         = tostring(local.budget)
-#     }
-#   }
-#   dynamic "threshold_rules" {
-#     for_each = toset([
-#       for i in range(0, 9) :
-#       tostring(1 + i * 0.05)
-#     ])
-#     content {
-#       threshold_percent = tonumber(threshold_rules.key)
-#     }
-#   }
-# }
+
+resource "google_monitoring_notification_channel" "wall_st" {
+  provider     = google.ca_e2
+  project      = local.gcp_project
+  display_name = "wall_st"
+  type         = "email"
+  labels = {
+    email_address = var.mein_email
+  }
+}
+
+resource "google_billing_budget" "septims" {
+  provider        = google.ca_e2
+  billing_account = var.gcp_billing_account
+  all_updates_rule {
+    disable_default_iam_recipients = true
+    monitoring_notification_channels = [
+      google_monitoring_notification_channel.wall_st.id
+    ]
+  }
+  amount {
+    specified_amount {
+      currency_code = "USD"
+      units         = tostring(local.budget)
+    }
+  }
+  dynamic "threshold_rules" {
+    for_each = toset([
+      for i in range(0, 1) :
+      tostring(1 + i * 0.05)
+    ])
+    content {
+      threshold_percent = tonumber(threshold_rules.key)
+    }
+  }
+}
