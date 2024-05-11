e6f2fa1e291ea565ea7efc1c925fdd5a9d10a5db
IIIIII <github@bigly.dog>
2024-05-10 21:09:15 -0400

    iam

--- terraform/bootstrap/aws.tf
+++ terraform/bootstrap/aws.tf
@@ -4,8 +4,25 @@ provider "aws" {
 data "aws_caller_identity" "whoami" {
 }
 
+resource "aws_iam_user" "sudo" {
+  name = "sudo"
+}
+
+resource "aws_iam_user" "s3" {
+  name = "s3"
+}
+
+resource "aws_iam_access_key" "pgp" {
+  for_each = toset([aws_iam_user.sudo.name, aws_iam_user.s3.name])
+  user     = each.key
+}
+
 output "aws" {
   value = {
-    whoami = data.aws_caller_identity.whoami
+    iam = {
+      root = data.aws_caller_identity.whoami.arn
+      sudo = aws_iam_user.sudo.arn
+      s3   = aws_iam_user.s3.arn
+    }
   }
 }
