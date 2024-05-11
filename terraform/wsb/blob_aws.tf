74c96f4dc18fee3b20ced4b1efd08d56a65ffac6
IIIIII <github@bigly.dog>
2024-05-10 23:08:29 -0400

    sudo

--- terraform/wsb/blob_aws.tf
+++ terraform/wsb/blob_aws.tf
@@ -1,3 +1,38 @@
+locals {
+  bastion_buckets = ["kfc-tfstate"]
+}
+
+resource "aws_iam_user" "s2" {
+  for_each = toset(["s2"])
+  name     = each.key
+}
+
+data "aws_iam_policy_document" "s2" {
+  statement {
+    actions   = ["s3:*"]
+    effect    = "Allow"
+    resources = ["arn:aws:s3:::*"]
+  }
+  statement {
+    actions = ["*"]
+    effect  = "Deny"
+    resources = flatten([
+      for bucket in local.bastion_buckets : [
+        "arn:aws:s3:::${bucket}",
+        "arn:aws:s3:::${bucket}/*"
+    ]])
+  }
+}
+
+resource "aws_iam_policy" "s2" {
+  policy = data.aws_iam_policy_document.s2.json
+}
+
+resource "aws_iam_access_key" "s2" {
+  for_each = toset([for _, user in aws_iam_user.s2 : user.name])
+  user     = each.key
+}
+
 resource "aws_s3_bucket" "kfc" {
   for_each      = toset(local.buckets)
   bucket        = "kfc-${each.key}"
