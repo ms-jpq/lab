7ac0c45e976cc4a20129e1897b56e13075378f43
IIIIII <github@bigly.dog>
2024-05-10 22:19:02 -0400

    sudo

--- terraform/bootstrap/aws.tf
+++ terraform/bootstrap/aws.tf
@@ -8,12 +8,50 @@ resource "aws_iam_user" "sudo" {
   name = "sudo"
 }
 
-resource "aws_iam_user" "s3" {
-  name = "s3"
+resource "aws_iam_user" "s2" {
+  name = "s2"
+}
+
+data "aws_iam_policy" "sudo" {
+  name = "AdministratorAccess"
+}
+
+locals {
+  bastion_buckets = [aws_s3_bucket.tfstate.bucket, "kfc-i6"]
+}
+
+data "aws_iam_policy_document" "s2" {
+  statement {
+    actions   = ["s3:*"]
+    effect    = "Allow"
+    resources = ["arn:aws:s3:::*"]
+  }
+  statement {
+    actions = ["*"]
+    effect  = "Deny"
+    resources = flatten([
+      for bucket in local.bastion_buckets : [
+        "arn:aws:s3:::${bucket}",
+        "arn:aws:s3:::${bucket}/*"
+    ]])
+  }
+}
+
+resource "aws_iam_policy" "s2" {
+  policy = data.aws_iam_policy_document.s2.json
+}
+
+resource "aws_iam_user_policy_attachment" "sudo" {
+  for_each = {
+    (aws_iam_user.sudo.name) = data.aws_iam_policy.sudo.arn
+    (aws_iam_user.s2.name)   = aws_iam_policy.s2.arn
+  }
+  user       = each.key
+  policy_arn = each.value
 }
 
 resource "aws_iam_access_key" "pgp" {
-  for_each = toset([aws_iam_user.sudo.name, aws_iam_user.s3.name])
+  for_each = toset([aws_iam_user.sudo.name, aws_iam_user.s2.name])
   user     = each.key
 }
 
@@ -28,7 +66,11 @@ output "aws" {
     iam = {
       root = data.aws_caller_identity.whoami.arn
       sudo = aws_iam_user.sudo.arn
-      s3   = aws_iam_user.s3.arn
+      s2   = aws_iam_user.s2.arn
+    }
+    keys = {
+      for key, access_key in aws_iam_access_key.pgp :
+      key => access_key.id
     }
   }
 }
