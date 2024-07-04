16f322aa9784c9f69a4597106f7c4d3436682c95
IIIIII <github@bigly.dog>
2024-07-04 11:04:07 -0400

    nginx

--- layers/_/usr/local/opt/cgi/bin/nginx.sh
+++ layers/_/usr/local/opt/cgi/bin/nginx.sh
@@ -14,6 +14,16 @@ EOF
 
 GIXY='/opt/python3/gixy'
 
+while read -r LINE; do
+  LINE="${LINE%$'\r'}"
+  if [[ -z $LINE ]]; then
+    break
+  fi
+  printf -- '%s\n' "> $LINE"
+done
+
+printf -- '\n\n'
+
 /usr/local/libexec/hr-run.sh curl --fail-with-body --location --unix /run/local/nginx/cgi.sock --no-progress-meter -- localhost/nginx
 /usr/local/libexec/hr-run.sh nginx -c /usr/local/opt/nginx/conf/main.nginx -T
 PYTHONPATH="$GIXY" /usr/local/libexec/hr-run.sh "$GIXY/bin/gixy" -- /usr/local/opt/nginx/conf/main.nginx
