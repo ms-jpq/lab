f7d8658a00372fba54c406a2259648f27ae968de
IIIIII <github@bigly.dog>
2024-07-04 11:24:05 -0400

    k

--- layers/_/usr/local/opt/cgi/bin/nginx.sh
+++ layers/_/usr/local/opt/cgi/bin/nginx.sh
@@ -24,6 +24,6 @@ done
 
 printf -- '\n\n'
 
+PYTHONPATH="$GIXY" /usr/local/libexec/hr-run.sh "$GIXY/bin/gixy" -- /usr/local/opt/nginx/conf/main.nginx
 /usr/local/libexec/hr-run.sh curl --fail-with-body --location --unix /run/local/nginx/cgi.sock --no-progress-meter -- localhost/nginx
 /usr/local/libexec/hr-run.sh nginx -c /usr/local/opt/nginx/conf/main.nginx -T
-PYTHONPATH="$GIXY" /usr/local/libexec/hr-run.sh "$GIXY/bin/gixy" -- /usr/local/opt/nginx/conf/main.nginx
