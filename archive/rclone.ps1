96bb5f461f261c6d158bc5effe4cd9af86c61e7c
IIIIII <github@bigly.dog>
2024-07-08 23:23:39 -0400

    dircache

--- archive/rclone.ps1
+++ archive/rclone.ps1
@@ -7,17 +7,17 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 Set-Location -- $PSScriptRoot
 
-$uri = 'http://freenas.enp2s0.pfsense.home.arpa:8080'
+$remote = 'http://freenas.enp2s0.pfsense.home.arpa:8080'
 $vol = 'rclone'
 $cache = 'D:\rclone.cache'
 $argv = @(
     '--config=NUL'
-    '--dir-cache-time=1s'
+    '--dir-cache-time=9s'
     '--network-mode'
     '--no-modtime',
     "--cache-dir=$cache"
     '--vfs-cache-mode=full'
-    "--http-url=$uri"
+    "--http-url=$remote"
     "--volname=$vol"
     'mount'
     '--'
