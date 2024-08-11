ca882632f727bc6b4f06fb00864b90d9d31718f1
IIIIII <github@bigly.dog>
2024-08-11 10:37:46 -0400

    wg ddns

---
+++ layers/router/usr/local/lib/systemd/system-generators/wg-ddns.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O globstar
+
+set -o pipefail
+
+RUN="$1"
+WANTS="$RUN/default.target.wants"
+
+NETDEVS='/usr/local/lib/systemd/network'
+SEARCH=(sed -E -n -e '/^\[WireGuard\]$/F' -- "$NETDEVS"/*.netdev)
+SELECT=(xargs -r -I % -- sed -E -n -e 's/^Name[[:space:]]*=[[:space:]]*(.*)$/\1/p' -- %)
+ROWS=$("${SEARCH[@]}" | "${SELECT[@]}")
+readarray -t -d '' -- NETDEVS <<< "$ROWS"
+
+mkdir -p -- "$WANTS"
+for NETDEV in "${NETDEVS[@]}"; do
+  NAME=$(systemd-escape -- "$NETDEV")
+  ln -sf -- /usr/local/lib/systemd/system/1-wg-ddns@.timer "$WANTS/1-wg-ddns@$NAME.timer"
+done
---
+++ layers/router/usr/local/opt/network/libexec/wg-ddhs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+NETDEV="$*"
+if CONF="$(wg showconf "$NETDEV")"; then
+  wg syncconf "$NETDEV" - <<< "$CONF"
+fi
