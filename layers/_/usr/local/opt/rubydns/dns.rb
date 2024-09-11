85bbabec1fc5c9ba76e8f23bb1d5eb38305e67ec
dogisgreat <github@bigly.dog>
2024-09-10 22:06:38 -0700

    dns

--- layers/_/usr/local/opt/rubydns/dns.rb
+++ layers/_/usr/local/opt/rubydns/dns.rb
@@ -1,34 +1,65 @@
 # frozen_string_literal: true
 
 module DNS
-  def srv_fail(query: nil)
-    Resolv::DNS::Message.new(query ? query.id : 0).tap do
-      _1.qr = 1
-      _1.opcode = query ? query.opcode : 0
-      _1.aa = 1
-      _1.rd = 0
-      _1.ra = 0
-      _1.rcode = Resolv::DNS::RCode::ServFail
+  def srv(sock:)
+    sock => Socket
+    root = Fiber.current
+    opt = sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_TYPE)
+
+    Fiber.new do
+      case opt.int
+      in Socket::SOCK_STREAM
+        loop do
+          sock.accept => [Socket => conn, Addrinfo]
+          len = conn.read(2).unpack1('n')
+          req = conn.read(len)
+          rsp = root.transfer(req)
+          conn.write(rsp)
+        ensure
+          conn&.close
+        end
+      in Socket::SOCK_DGRAM
+        loop do
+          sock.recvfrom(Resolv::DNS::UDPSize) => [
+            String => req,
+            Addrinfo => addr
+          ]
+          ai = Socket.sockaddr_in(addr.ip_port, addr.ip_address)
+          rsp = root.transfer(req)
+          sock.send(rsp, 0, ai)
+        end
+      end
     end
   end
 
-  def parse(msg:)
-    msg => String
-    Resolv::DNS::Message.decode(msg)
-  rescue StandardError => e
-    Logger.error(e)
-    srv_fail(query:)
+  def srv_fail(query: nil)
+    query => Resolv::DNS::Message | nil
+    Resolv::DNS::Message
+      .new(query&.id || 0)
+      .tap do
+        _1.qr = 1
+        _1.opcode = query&.opcode || 0
+        _1.aa = 1
+        _1.rd = 0
+        _1.ra = 0
+        _1.rcode = Resolv::DNS::RCode::ServFail
+      end
+      .encode
   end
 
   def query(dns:, msg:)
-    parse(msg:) => Resolv::DNS::Message => query
+    msg => String
+    Resolv::DNS::Message.decode(msg) => Resolv::DNS::Message => query
     rsp = Resolv::DNS::Message.new(query.id)
     query.each_question do |name, typeclass|
-      [name, typeclass] => [Resolv::DNS::Name, Resolv::DNS::Resource]
-      dns.getresources(name, typeclass).each do
-        rsp.add_answer(name, _1.ttl, _1)
-      end
+      name => Resolv::DNS::Name
+      dns
+        .getresources(name, typeclass)
+        .each { rsp.add_answer(name, _1.ttl, _1) }
     end
     rsp.encode
+  rescue StandardError => e
+    Logger.new($stderr).error(e)
+    srv_fail(query:)
   end
 end
--- layers/_/usr/local/opt/rubydns/main.rb
+++ layers/_/usr/local/opt/rubydns/main.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S -- ruby
 # frozen_string_literal: true
 
+require('English')
 require('abbrev')
 require('etc')
 require('logger')
@@ -9,7 +10,6 @@ require('resolv')
 require('socket')
 
 require_relative('dns')
-puts(Process.pid)
 
 Thread.tap { _1.abort_on_exception = true }
 
@@ -35,65 +35,28 @@ sockets =
     _1.split(':', 2) => [p, addr]
     [proto.fetch(p), addr]
   end
-  .flat_map do
-    case _1
-    in [:udp, addr]
-      Socket.udp_server_sockets(addr)
-    in [:tcp, addr]
-      Socket.tcp_server_sockets(addr)
+    .flat_map do
+      case _1
+      in [:udp, addr]
+        Socket.udp_server_sockets(addr)
+      in [:tcp, addr]
+        Socket.tcp_server_sockets(addr)
+      end
     end
-  end
-  .to_a
+    .to_a
 
-tx =
-  sockets.map do |sock|
-    sock => Socket
-    opt = sock.getsockopt(Socket::SOL_SOCKET, Socket::SO_TYPE)
-
-    Fiber.new do
-      case opt.int
-      in Socket::SOCK_STREAM
+sockets
+  .map do |sock|
+    Thread.new do
+      extend(DNS)
+      fib = srv(sock:)
+      Resolv::DNS.open do |dns|
+        msg = fib.transfer
         loop do
-          sock.accept => [Socket => conn, Addrinfo]
-          len = conn.read(2).unpack1('n')
-          req = conn.read(len)
-          rsp = Fiber.yield(req)
-          conn.write(rsp)
-        ensure
-          conn&.close
-        end
-      in Socket::SOCK_DGRAM
-        loop do
-          sock.recvfrom(Resolv::DNS::UDPSize) => [String => req, Addrinfo => addr]
-          ai = Socket.sockaddr_in(addr.ip_port, addr.ip_address)
-          rsp = Fiber.yield(req)
-          sock.send(rsp, 0, ai)
+          rsp = query(dns:, msg:)
+          msg = fib.transfer(rsp)
         end
       end
     end
   end
-
-Resolv::DNS.open do |dns|
-  loop do
-    case Ractor.select(*tx, move: true)
-    in [_, nil]
-      break
-    in [Ractor, Socket => conn]
-      begin
-        query(dns:, msg:)
-        conn.write(query)
-      rescue IOError => e
-        Logger.error(e)
-      ensure
-        conn.close
-      end
-    in [Ractor, [Socket => sock, String => ai, String => msg]]
-      query = query(dns:, msg:)
-      begin
-        sock.send(query, 0, ai)
-      rescue IOError => e
-        Logger.error(e)
-      end
-    end
-  end
-end
+  .each(&:join)
