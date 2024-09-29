6557eed1cddcf1f4457e54546aa28159a6427c93
dogisgreat <github@bigly.dog>
2024-09-29 11:20:02 -0400

    letter q

--- terraform/wtc7/email_exec.tf
+++ terraform/wtc7/email_exec.tf
@@ -9,7 +9,7 @@ resource "aws_lambda_function" "mta" {
   architectures    = ["arm64"]
   filename         = data.archive_file.mta.output_path
   function_name    = basename(data.archive_file.mta.source_dir)
-  handler          = "handler"
+  handler          = "main"
   role             = aws_iam_role.mta.arn
   runtime          = "python3.12"
   source_code_hash = data.archive_file.mta.output_base64sha256
@@ -34,7 +34,7 @@ resource "aws_cloudwatch_event_rule" "cron" {
 
 resource "aws_cloudwatch_event_target" "cron" {
   provider = aws.us_e1
-  arn      = aws_lambda_function.process_dlq.arn
+  arn      = aws_lambda_function.mta.arn
   rule     = aws_cloudwatch_event_rule.cron.name
 
   dead_letter_config {
--- terraform/wtc7/mta/main.py
+++ terraform/wtc7/mta/main.py
@@ -1,2 +1,33 @@
-def mta() -> str:
-    return "MTA"
+from collections.abc import Mapping
+from typing import cast
+
+from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.utilities.batch import (
+    AsyncBatchProcessor,
+    EventType,
+    async_process_partial_response,
+)
+from aws_lambda_powertools.utilities.data_classes import SQSEvent, event_source
+from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSRecord
+from aws_lambda_powertools.utilities.parser.models import SqsRecordModel
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+log, trace = Logger(), Tracer()
+
+
+@trace.capture_method
+async def _run(record: SQSRecord) -> None:
+    log.info(record.body)
+
+
+@log.inject_lambda_context
+@trace.capture_lambda_handler
+@event_source(data_class=SQSEvent)
+def main(event: SQSEvent, ctx: LambdaContext) -> Mapping:
+    processor = AsyncBatchProcessor(event_type=EventType.SQS, model=SqsRecordModel)
+    return async_process_partial_response(
+        cast(dict, event),
+        context=ctx,
+        processor=processor,
+        record_handler=_run,
+    )
