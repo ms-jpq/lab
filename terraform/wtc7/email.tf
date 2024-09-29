eb2242d50471a900663c01c64f2a2cee9c713f75
dogisgreat <github@bigly.dog>
2024-09-29 07:52:06 -0400

    mv

--- terraform/wtc7/email.tf
+++ terraform/wtc7/email.tf
@@ -11,35 +11,6 @@ resource "aws_sqs_queue" "dns" {
   provider = aws.us_e1
 }
 
-data "aws_iam_policy_document" "mbox" {
-  statement {
-    actions   = ["s3:PutObject"]
-    effect    = "Allow"
-    resources = ["${aws_s3_bucket.maildir.arn}/*"]
-  }
-  statement {
-    actions   = ["sqs:SendMessage"]
-    effect    = "Allow"
-    resources = [aws_sqs_queue.mbox.arn]
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    condition {
-      test     = "ArnEquals"
-      variable = "aws:SourceArn"
-      values   = [aws_s3_bucket.maildir.arn]
-    }
-  }
-}
-
-resource "aws_sqs_queue_policy" "mbox" {
-  provider  = aws.us_e1
-  queue_url = aws_sqs_queue.mbox.id
-  policy    = data.aws_iam_policy_document.mbox.json
-}
-
 data "aws_route53_zone" "limited_void" {
   name = replace(regex("@.+$", var.mail_from), "@", "")
 }
@@ -79,29 +50,6 @@ resource "aws_ses_receipt_rule_set" "maildir" {
   rule_set_name = "maildir"
 }
 
-data "aws_iam_policy_document" "maildir" {
-  statement {
-    actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.maildir.arn}/*"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ses.amazonaws.com"]
-    }
-    condition {
-      test     = "ArnEquals"
-      variable = "aws:SourceArn"
-      values   = [aws_ses_receipt_rule_set.maildir.arn]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "maildir" {
-  provider = aws.us_e1
-  bucket   = aws_s3_bucket.maildir.id
-  policy   = data.aws_iam_policy_document.maildir.json
-}
-
 resource "aws_ses_receipt_rule" "maildir" {
   provider      = aws.us_e1
   depends_on    = [aws_s3_bucket_policy.maildir]
@@ -129,54 +77,6 @@ resource "aws_s3_bucket_notification" "maildir" {
   }
 }
 
-data "aws_iam_policy_document" "mta" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "port_auth" {
-  statement {
-    actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
-    effect    = "Allow"
-    resources = ["arn:aws:logs:::"]
-  }
-  statement {
-    actions   = ["sqs:ReceiveMessage", "sqs:DeleteMessage", "sqs:GetQueueAttributes"]
-    effect    = "Allow"
-    resources = [aws_sqs_queue.mbox.arn]
-  }
-  statement {
-    actions   = ["s3:GetObject", "s3:PutObject"]
-    effect    = "Allow"
-    resources = ["${aws_s3_bucket.maildir.arn}/*"]
-  }
-  statement {
-    actions   = ["ses:SendRawEmail"]
-    effect    = "Allow"
-    resources = ["*"]
-  }
-}
-
-resource "aws_iam_role" "mta" {
-  assume_role_policy = data.aws_iam_policy_document.mta.json
-}
-
-resource "aws_iam_policy" "port_auth" {
-  policy = data.aws_iam_policy_document.port_auth.json
-}
-
-resource "aws_iam_role_policy_attachment" "port_auth" {
-  role       = aws_iam_role.mta.name
-  policy_arn = aws_iam_policy.port_auth.arn
-}
-
 data "archive_file" "mta" {
   output_path = "${path.module}/../../var/mta.zip"
   source_dir  = "${path.module}/mta"
@@ -194,33 +94,9 @@ resource "aws_lambda_function" "mta" {
   source_code_hash = data.archive_file.mta.output_base64sha256
 }
 
-data "aws_iam_policy_document" "dns" {
-  statement {
-    actions   = ["sqs:SendMessage"]
-    effect    = "Allow"
-    resources = [aws_sqs_queue.dns.arn]
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    condition {
-      test     = "ArnEquals"
-      variable = "aws:SourceArn"
-      values   = [aws_lambda_function.mta.arn]
-    }
-  }
-}
-
-resource "aws_sqs_queue_policy" "dns" {
-  provider  = aws.us_e1
-  queue_url = aws_sqs_queue.dns.id
-  policy    = data.aws_iam_policy_document.dns.json
-}
-
 resource "aws_lambda_function_event_invoke_config" "mta" {
   provider      = aws.us_e1
-  depends_on    = [aws_sqs_queue_policy.dns]
+  depends_on    = [aws_sqs_queue_policy.qq]
   function_name = aws_lambda_function.mta.function_name
 
   destination_config {
---
+++ terraform/wtc7/email_policy.tf
@@ -0,0 +1,121 @@
+data "aws_iam_policy_document" "maildir" {
+  statement {
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.maildir.arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ses.amazonaws.com"]
+    }
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_ses_receipt_rule_set.maildir.arn]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "mbox" {
+  statement {
+    actions   = ["s3:PutObject"]
+    effect    = "Allow"
+    resources = ["${aws_s3_bucket.maildir.arn}/*"]
+  }
+  statement {
+    actions   = ["sqs:SendMessage"]
+    effect    = "Allow"
+    resources = [aws_sqs_queue.mbox.arn]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.maildir.arn]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "dns" {
+  statement {
+    actions   = ["sqs:SendMessage"]
+    effect    = "Allow"
+    resources = [aws_sqs_queue.dns.arn]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_lambda_function.mta.arn]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "mta" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "port_auth" {
+  statement {
+    actions   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+    effect    = "Allow"
+    resources = ["arn:aws:logs:::"]
+  }
+  statement {
+    actions   = ["sqs:ReceiveMessage", "sqs:DeleteMessage", "sqs:GetQueueAttributes"]
+    effect    = "Allow"
+    resources = [aws_sqs_queue.mbox.arn]
+  }
+  statement {
+    actions   = ["s3:GetObject", "s3:PutObject"]
+    effect    = "Allow"
+    resources = ["${aws_s3_bucket.maildir.arn}/*"]
+  }
+  statement {
+    actions   = ["ses:SendRawEmail"]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+}
+
+resource "aws_s3_bucket_policy" "maildir" {
+  provider = aws.us_e1
+  bucket   = aws_s3_bucket.maildir.id
+  policy   = data.aws_iam_policy_document.maildir.json
+}
+
+resource "aws_sqs_queue_policy" "qq" {
+  provider = aws.us_e1
+  for_each = {
+    (aws_sqs_queue.mbox.id) : data.aws_iam_policy_document.mbox,
+    (aws_sqs_queue.dns.id) : data.aws_iam_policy_document.dns
+  }
+  queue_url = each.key
+  policy    = each.value.json
+}
+
+resource "aws_iam_role" "mta" {
+  assume_role_policy = data.aws_iam_policy_document.mta.json
+}
+
+resource "aws_iam_policy" "port_auth" {
+  policy = data.aws_iam_policy_document.port_auth.json
+}
+
+resource "aws_iam_role_policy_attachment" "port_auth" {
+  role       = aws_iam_role.mta.name
+  policy_arn = aws_iam_policy.port_auth.arn
+}
