78a51d0af3c4750e25445ae072e48ca2a09deeb2
dogisgreat <github@bigly.dog>
2024-09-29 19:02:12 -0400

    info

--- terraform/wtc7/mta/mta.py
+++ terraform/wtc7/mta/mta.py
@@ -7,7 +7,6 @@ from aws_lambda_powertools.utilities.batch import (
     EventType,
     async_process_partial_response,
 )
-from aws_lambda_powertools.utilities.data_classes import SQSEvent, event_source
 from aws_lambda_powertools.utilities.data_classes.sqs_event import SQSRecord
 from aws_lambda_powertools.utilities.parser.models import SqsRecordModel
 from aws_lambda_powertools.utilities.typing import LambdaContext
@@ -25,17 +24,16 @@ async def _run(record: SQSRecord) -> None:
     #     Destination={"CcAddresses": [""]},
     #     Content={"Raw": {"Data": data}},
     # )
-    log.info(record.body)
+    log.error(record)
 
 
 @metrics.log_metrics
 @log.inject_lambda_context
 @trace.capture_lambda_handler
-@event_source(data_class=SQSEvent)
-def main(event: SQSEvent, ctx: LambdaContext) -> Mapping[str, Any]:
+def main(event: dict[Any, Any], ctx: LambdaContext) -> Mapping[str, Any]:
     processor = AsyncBatchProcessor(event_type=EventType.SQS, model=SqsRecordModel)
     return async_process_partial_response(
-        cast(dict[Any, Any], event),
+        event,
         context=ctx,
         processor=processor,
         record_handler=_run,
