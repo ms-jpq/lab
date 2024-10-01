5d510d571af98fa65bed2bb71a5bf902bf66896a
dogisgreat <github@bigly.dog>
2024-10-01 14:50:40 -0400

    log

--- terraform/wtc7/mta/sieve.py
+++ terraform/wtc7/mta/sieve.py
@@ -1,9 +1,11 @@
 from email.message import EmailMessage
 from email.utils import getaddresses, parseaddr
+from logging import getLogger
 
 
 def sieve(msg: EmailMessage) -> bool:
-    m_from = parseaddr(msg.get("from", ""))
-    m_to = parseaddr(msg.get("to", ""))
-    m_cc = getaddresses((msg.get("cc", ""),))
+    m_name, m_from = parseaddr(msg.get("from", ""))
+    _, m_to = parseaddr(msg.get("to", ""))
+    m_cnames, m_cc = zip(*getaddresses([msg.get("cc", "")]))
+    getLogger().debug(f"from: {m_from}, to: {m_to}, cc: {m_cc}")
     return True
