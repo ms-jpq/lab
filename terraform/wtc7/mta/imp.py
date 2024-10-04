3f3840f86a62c797fb75b9be46c226bf25b49738
dogisgreat <github@bigly.dog>
2024-10-03 20:30:14 -0400

    specyes

--- terraform/wtc7/mta/imp.py
+++ terraform/wtc7/mta/imp.py
@@ -1,45 +1,53 @@
 from collections.abc import MutableSequence, Sequence
-from functools import cache
 from http.client import HTTPResponse
 from importlib.abc import Loader, MetaPathFinder
 from importlib.machinery import ModuleSpec
+from importlib.util import LazyLoader, spec_from_loader
 from os import linesep
+from sys import meta_path
 from types import ModuleType
+from typing import cast
+from urllib.error import URLError
 from urllib.request import build_opener
 
-TIMEOUT = 6.9
 
+def register(name: str, uri: str, retries: int, timeout: float) -> None:
+    def get() -> bytes:
+        opener = build_opener()
+        errs: MutableSequence[Exception] = []
+        for _ in range(retries):
+            try:
+                with opener.open(uri, timeout=timeout) as req:
+                    assert isinstance(req, HTTPResponse)
+                    return req.read()
+            except URLError as exn:
+                errs.append(exn)
+        else:
+            msg = linesep.join(map(str, errs))
+            *_, e = errs
+            raise ExceptionGroup(msg, errs) from e
 
-@cache
-def _script(uri: str) -> str:
-    opener = build_opener()
-    errs: MutableSequence[Exception] = []
-    for _ in range(3):
-        try:
-            with opener.open(uri, timeout=TIMEOUT) as req:
-                assert isinstance(req, HTTPResponse)
-                return req.read().decode()
-        except Exception as exn:
-            errs.append(exn)
-    else:
-        msg = linesep.join(map(str, errs))
-        *_, e = errs
-        raise ExceptionGroup(msg, errs) from e
+    class _Finder(MetaPathFinder):
+        def find_spec(
+            self,
+            fullname: str,
+            path: Sequence[str] | None = None,
+            target: ModuleType | None = None,
+        ) -> ModuleSpec | None:
+            if fullname != name:
+                return None
 
+            class _Loader(Loader):
+                def create_module(self, spec: ModuleSpec) -> ModuleType | None:
+                    return None
 
-class _Loader(Loader):
-    def create_module(self, spec: ModuleSpec) -> ModuleType | None:
-        return None
+                def exec_module(self, module: ModuleType) -> None:
+                    src = get()
+                    code = compile(src, fullname, "exec")
+                    exec(code, module.__dict__)
 
-    def exec_module(self, module: ModuleType) -> None:
-        pass
+            loader = LazyLoader.factory(cast(Loader, _Loader))
+            spec = spec_from_loader(fullname, loader())
+            return spec
 
-
-class _Finder(MetaPathFinder):
-    def find_spec(
-        self,
-        fullname: str,
-        path: Sequence[str] | None = None,
-        target: ModuleType | None = None,
-    ) -> ModuleSpec | None:
-        return None
+    meta_path.append(_Finder())
