1b63cf2c697a3e5d5da8569cd707497624aa1a46
dogisgreat <github@bigly.dog>
2024-10-27 10:56:58 -0400

    -

--- libexec/kompose.sh
+++ libexec/kompose.sh
@@ -9,7 +9,8 @@ DST="./var/tmp/k8s/$SRC"
 COMPOSE="var/tmp/machines/$SRC/fs/usr/local/k8s"
 
 gmake MACHINE="$SRC" local
-mkdir -v -p -- "$DST"
+rm -fr -- "$DST"
+mkdir -p -- "$DST"
 
 DENV='./var/sh/zsh/dev/bin/denv.py'
 KOMPOSE='./var/bin/kompose'
@@ -30,7 +31,7 @@ for FILE in "$COMPOSE"/*/docker-compose.yml; do
   touch -- "$ENV"
   {
     printf -- '%s\n' "# $FILE"
-    "$DENV" -- "$ENV" "$KOMPOSE" convert --stdout --namespace "$STACK" --file "$FILE" -v
+    "$DENV" -- "$ENV" "$KOMPOSE" convert --stdout --namespace "$STACK" --file "$FILE"
   } > "$DST/$STACK.yml"
 done
 printf -- '%s\n' "<<< $DST" >&2
