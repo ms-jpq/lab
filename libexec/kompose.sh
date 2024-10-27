3d16139ccb78f5333a6428337928bd916e817250
dogisgreat <github@bigly.dog>
2024-10-27 08:48:28 -0400

    kompose

--- libexec/kompose.sh
+++ libexec/kompose.sh
@@ -4,8 +4,9 @@ set -o pipefail
 
 cd -- "${0%/*}/.."
 
-OUT='./var/compose'
-mkdir -v -p -- "$OUT"
+SRC="$1"
+DST='./var/compose'
+mkdir -v -p -- "$DST"
 
 Y2J=(yq --output-format json)
 J2Y=(yq --input-format json)
@@ -16,8 +17,9 @@ MOD=(jq '.metadata.namespace = $name')
 for FILE in ./containers/*/docker-compose.yml; do
   NAME="${FILE%/*}"
   NAME="${NAME##*/}"
+  printf -- '%s\n' ">>> $NAME <<<"
   {
     K8S_NAMESPACE="$NAME" envsubst < ./containers/namespace.k8s.yml
-    ./var/bin/kompose convert --stdout --file "$FILE" | "${Y2J[@]}" | "${MOD[@]}" --arg name "$NAME" | "${J2Y[@]}"
-  } > "$OUT/$NAME.yml"
+    ./var/bin/kompose convert --stdout --namespace "$NAME" --file "$FILE"
+  } > "$DST/$NAME.yml"
 done
