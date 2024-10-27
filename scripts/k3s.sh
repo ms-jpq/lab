13f7fec7509b55a7dd7d52bed43930e0f54c8d5a
dogisgreat <github@bigly.dog>
2024-10-27 08:30:42 -0400

    always restart

--- scripts/k3s.sh
+++ scripts/k3s.sh
@@ -7,11 +7,17 @@ cd -- "${0%/*}/.."
 OUT='./var/compose'
 mkdir -v -p -- "$OUT"
 
+Y2J=(yq --output-format json)
+J2Y=(yq --input-format json)
+
+# shellcheck disable=SC2016
+MOD=(jq '.metadata.namespace = $name')
+
 for FILE in ./containers/*/docker-compose.yml; do
   NAME="${FILE%/*}"
   NAME="${NAME##*/}"
   {
     K8S_NAMESPACE="$NAME" envsubst < ./containers/namespace.k8s.yml
-    ./var/bin/kompose convert --stdout --file "$FILE"
-  } > "$OUT/$NAME.json"
+    ./var/bin/kompose convert --stdout --file "$FILE" | "${Y2J[@]}" | "${MOD[@]}" --arg name "$NAME" | "${J2Y[@]}"
+  } > "$OUT/$NAME.yml"
 done
