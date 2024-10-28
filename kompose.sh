e84e7958b1321fe647e3c420712265ef0815cad3
dogisgreat <github@bigly.dog>
2024-10-28 07:30:31 -0400

    kompse

--- kompose.sh
+++ kompose.sh
@@ -6,14 +6,14 @@ cd -- "${0%/*}"
 
 SRC="$1"
 DST="./var/tmp/k8s/$SRC"
-COMPOSE="var/tmp/machines/$SRC/fs/usr/local/k8s"
+COMPOSE="./var/tmp/machines/$SRC/fs/usr/local/k8s"
 KOMPOSE='var/bin/kompose'
-DENV='var/sh/zsh/dev/bin/denv.py'
+DENV='./var/sh/zsh/dev/bin/denv.py'
 
 gmake "$KOMPOSE"
 gmake MACHINE="$SRC" local
+rm -fr -- "$DST"
 mkdir -p -- "$DST"
-rm -fr -- "${DST:?}"/*
 
 Y2J=(yq --output-format json)
 J2Y=(yq --input-format json '(.. | select(tag == "!!str")) style="single"')
@@ -25,12 +25,15 @@ JQ
 printf -- '%s\n' ">>> $COMPOSE" >&2
 for FILE in "$COMPOSE"/*/docker-compose.yml; do
   DIR="${FILE%/*}"
-  STACK="${DIR##*/}"
+  NAMESPACE="${DIR##*/}"
   ENV="$DIR/.env"
 
-  printf -- '%s\n' "@ $STACK" >&2
+  printf -- '%s\n' "@ $NAMESPACE" >&2
   touch -- "$ENV"
-  CONV=("$DENV" -- "$ENV" "$KOMPOSE" convert --stdout --generate-network-policies --namespace "$STACK" --file "$FILE")
-  "${CONV[@]}" | "${Y2J[@]}" | jq --slurp "$JQ" | "${J2Y[@]}" > "$DST/$STACK.yml"
+  CONV=("$DENV" -- "$ENV" "$KOMPOSE" convert --stdout --generate-network-policies --namespace "$NAMESPACE" --file "$FILE")
+  {
+    "${CONV[@]}" | "${Y2J[@]}" | jq --slurp "$JQ" | "${J2Y[@]}"
+    NAMESPACE="$NAMESPACE" envsubst < './layers/k3s/usr/local/k8s/networkpolicy.k8s.yml'
+  } > "$DST/$NAMESPACE.yml"
 done
 printf -- '%s\n' "<<< $DST" >&2
