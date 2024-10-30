320a359180be3a28fe247a2ff8074bbcf4039efa
dogisgreat <github@bigly.dog>
2024-10-30 01:00:16 -0400

    a

--- kustomize.sh
+++ kustomize.sh
@@ -2,24 +2,49 @@
 
 set -o pipefail
 
+OPTS='d,p'
+LONG_OPTS='diff,prune'
+GO="$(getopt --options="$OPTS" --longoptions="$LONG_OPTS" --name="$0" -- "$@")"
+eval -- set -- "$GO"
+
+DIFF=0
+PRUNE=()
+while (($#)); do
+  case "$1" in
+  -d | --diff)
+    DIFF=1
+    shift -- 1
+    ;;
+  -p | --prune)
+    PRUNE=(--prune --all)
+    shift -- 1
+    ;;
+  --)
+    SRC="$2"
+    DST="./var/tmp/k8s/$SRC"
+    shift -- 2
+    break
+    ;;
+  *)
+    exit 1
+    ;;
+  esac
+done
+
 cd -- "${0%/*}"
 
-SRC="$1"
-DST="./var/tmp/k8s/$SRC"
-shift -- 1
-
 mkdir -p -- "$DST"
-rm -fr -- "${DST:?}"/*
+if ! (($#)); then
+  rm -fr -- "${DST:?}"/*
+fi
 
 ./libexec/kompose.sh "$SRC" "$DST" "$@"
 ./libexec/helm-charts.sh "$DST"
 
-if (($#)); then
-  PRUNE=()
+if ((DIFF)); then
+  for YML in "${DST}"/*.yml; do
+    ./libexec/kubectl.sh diff --filename "$YML"
+  done | delta
 else
-  PRUNE=(--prune --all)
-fi
-
-if ! [[ -v DRY ]]; then
   cat -- "$DST"/*.yml | ./libexec/kubectl.sh apply "${PRUNE[@]}" --filename -
 fi
