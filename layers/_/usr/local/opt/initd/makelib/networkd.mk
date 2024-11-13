4f3fd3b90b779ff7a6a34104558bdfee8ac2c580
dogisgreat <github@bigly.dog>
2024-11-12 20:01:30 -0500

    k

---
+++ layers/_/usr/local/opt/initd/makelib/networkd.mk
@@ -0,0 +1,14 @@
+/usr/lib/systemd/network: pkg._
+
+define SYSTEMD_CHOWN_TEMPLATE
+all: $1/._touch
+$1/._touch: $(shell shopt -u failglob && printf -- '%s ' $1/**/!(._touch)) | /usr/lib/systemd/network
+	sudo -- chown -R -- $2:$2 '$$(@D)'
+	sudo -- touch -- '$$@'
+endef
+
+$(eval $(call SYSTEMD_CHOWN_TEMPLATE,/usr/local/opt/network,systemd-network))
+$(eval $(call SYSTEMD_CHOWN_TEMPLATE,/usr/local/lib/systemd/network,systemd-network))
+$(eval $(call SYSTEMD_CHOWN_TEMPLATE,/usr/local/lib/systemd/networkd.conf.d,systemd-network))
+$(eval $(call SYSTEMD_CHOWN_TEMPLATE,/usr/local/lib/systemd/dnssd,systemd-resolve))
+$(eval $(call SYSTEMD_CHOWN_TEMPLATE,/usr/local/lib/systemd/resolved.conf.d,systemd-resolve))
