37582cc2760ac507a39d3c21fa682560c122a823
dogisgreat <github@bigly.dog>
2024-11-13 09:05:42 -0500

    do.tf

---
+++ terraform/wsb/do.tf
@@ -0,0 +1,18 @@
+data "digitalocean_regions" "abbottabad" {
+  filter {
+    key    = "available"
+    values = ["true"]
+  }
+  filter {
+    key      = "slug"
+    match_by = "re"
+    values   = ["^nyc"]
+  }
+}
+
+locals {
+  do_regions = toset([
+    for region in data.digitalocean_regions.abbottabad.regions :
+    region.slug
+  ])
+}
--- terraform/wtc5/do.tf
+++ terraform/wtc5/do.tf
@@ -1,18 +1,1 @@
-data "digitalocean_regions" "abbottabad" {
-  filter {
-    key    = "available"
-    values = ["true"]
-  }
-  filter {
-    key      = "slug"
-    match_by = "re"
-    values   = ["^nyc"]
-  }
-}
-
-locals {
-  do_regions = toset([
-    for region in data.digitalocean_regions.abbottabad.regions :
-    region.slug
-  ])
-}
+../wsb/do.tf
