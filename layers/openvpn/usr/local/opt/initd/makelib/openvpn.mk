d59d800eb8f49c1e44fed3d531b26303bd1861c2
dogisgreat <github@bigly.dog>
2024-11-12 20:29:09 -0500

    k

--- layers/openvpn/usr/local/opt/initd/makelib/openvpn.mk
+++ layers/openvpn/usr/local/opt/initd/makelib/openvpn.mk
@@ -1,9 +1,9 @@
 .PHONY: openvpn
 all: openvpn
 
-$(eval $(call SYSTEMD_CHOWN_TEMPLATE,/usr/local/opt/openvpn,systemd-network))
-
 /usr/lib/systemd/system/openvpn@.service: | pkg._
 openvpn: /usr/local/lib/systemd/system/1-ovpn-udp@.service /usr/local/lib/systemd/system/1-ovpn-tcp@.service
 /usr/local/lib/systemd/system/1-ovpn-udp@.service /usr/local/lib/systemd/system/1-ovpn-tcp@.service: /usr/lib/systemd/system/openvpn@.service
 	sudo -- cp -v -f -- '$<' '$@'
+
+$(eval $(call SYSTEMD_CHOWN_TEMPLATE,/usr/local/opt/openvpn,systemd-network))
