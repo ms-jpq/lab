dcf0267cf37591d7fa805c55e1977c16c36189a8
dogisgreat <github@bigly.dog>
2024-11-16 05:55:54 -0500

    k

--- layers/k3s/usr/local/opt/cgi/bin/k3s.sh
+++ layers/k3s/usr/local/opt/cgi/bin/k3s.sh
@@ -6,13 +6,29 @@ if [[ ! -t 1 ]]; then
   exec <&3 >&3
 fi
 
+HOST="$HOSTNAME"
+while read -r LINE; do
+  LINE="${LINE%$'\r'}"
+  if [[ -z $LINE ]]; then
+    break
+  fi
+
+  LHS="${LINE%%:*}"
+  KEY="${LHS,,}"
+  case "$KEY" in
+  host)
+    HOST="${LINE##*: }"
+    ;;
+  *) ;;
+  esac
+done
+
 tee -- <<- 'EOF'
 HTTP/1.0 200 OK
 Content-Type: text/plain; charset=utf-8
 
 EOF
 
-/usr/local/libexec/hr-run.sh k3s kubectl get --all-namespaces all
+/usr/local/libexec/hr-run.sh cat -- /var/lib/k3s/kubeconfig.yml | sed -E -e "s#https://\[::\]:6443#https://$HOST:6443#g"
 /usr/local/libexec/hr-run.sh systemctl --no-pager --full --lines 0 status -- 0-k3s.service
 /usr/local/libexec/hr-run.sh systemctl --no-pager --full --lines 0 status -- kubepods.slice
-/usr/local/libexec/hr-run.sh cat -- /var/lib/k3s/kubeconfig.yml
---
+++ layers/k3s/usr/local/opt/cgi/bin/kubectl.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+if [[ ! -t 1 ]]; then
+  exec <&3 >&3
+fi
+
+tee -- <<- 'EOF'
+HTTP/1.0 200 OK
+Content-Type: text/plain; charset=utf-8
+
+EOF
+
+/usr/local/libexec/hr-run.sh k3s kubectl get --all-namespaces all
