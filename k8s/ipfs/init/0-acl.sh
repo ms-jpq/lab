2f86f0901b4b246e0f273a2f161285a11ca16cde
dogisgreat <github@bigly.dog>
2024-11-17 21:18:55 -0500

    remove ipfs

--- k8s/ipfs/init/0-acl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-# shellcheck disable=2154
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"https://$ENV_CORS_DOMAIN\"]"
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'
