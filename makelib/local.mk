67580114dd8e6bbe16c8b6937edb867d2ec5558c
dogisgreat <github@bigly.dog>
2024-11-19 08:47:22 -0500

    local

--- makelib/local.mk
+++ makelib/local.mk
@@ -105,6 +105,7 @@ define REF_LINKS
 
 layers/posix/home/.config/bottom/bottom.toml                                                   root/.config/bottom/bottom.toml
 layers/posix/home/.config/lf/lfrc                                                              root/.config/lf/lfrc
+layers/posix/home/.config/lf/preview.sh                                                        root/.config/lf/preview.sh
 layers/posix/home/.local/opt/initd/libexec/curl-unpack.sh                                      usr/local/opt/initd/libexec/curl-unpack.sh
 layers/posix/home/.local/opt/initd/libexec/pkg.sh                                              usr/local/opt/initd/libexec/pkg.sh
 layers/posix/home/.local/opt/qemu/cloud-init                                                   usr/local/opt/qemu/cloud-init
