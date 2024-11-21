a338085e5a38e9100cfb4b9bd72a72bf7b3d6a15
dogisgreat <github@bigly.dog>
2024-11-21 06:57:07 -0500

    warp

---
+++ layers/cloudflare-warp/usr/local/opt/initd/makelib/cloudflare.mk
@@ -0,0 +1,3 @@
+pkg._: /etc/apt/trusted.gpg.d/cloudflare.gpg
+/etc/apt/trusted.gpg.d/cloudflare.gpg:
+	$(CURL) -- 'https://pkg.cloudflareclient.com/pubkey.gpg' | sudo -- gpg --batch --dearmor --yes --output '$@'
