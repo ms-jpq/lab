37f06f7add072cd30c5fb8af591af72992a95bef
dogisgreat <hao@stainlessapi.com>
2024-11-21 10:24:57 -0500

    exec

--- layers/_/usr/local/opt/initd/makelib/exec.mk
+++ layers/_/usr/local/opt/initd/makelib/exec.mk
@@ -1,5 +1,7 @@
 .PHONY: reload
 
 reload: | all
+	shopt -u failglob
 	sudo -- systemctl daemon-reload --no-pager --show-transaction
 	sudo -- systemctl --no-pager --show-transaction --failed --no-block -- restart '*'
+	sudo -- systemctl --no-pager --show-transaction --failed --no-block -- start /run/systemd/generator/multi-user.target.wants/*
