806806bd02bd57cfc5a72c7910b740d745f598ba
dogisgreat <github@bigly.dog>
2024-11-29 14:43:08 -0400

    k

--- makelib/k8s.mk
+++ makelib/k8s.mk
@@ -7,5 +7,6 @@ k8s: helm
 helm: $(VAR)/helm/config/repositories.yaml
 $(VAR)/helm/config/repositories.yaml: | $(VAR)/bin/helm
 	./libexec/helm.sh '' repo add keel     'https://charts.keel.sh'
+	./libexec/helm.sh '' repo add nvdp     'https://nvidia.github.io/k8s-device-plugin'
 	./libexec/helm.sh '' repo add stakater 'https://stakater.github.io/stakater-charts'
 	./libexec/helm.sh '' repo update
