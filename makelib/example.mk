0398a2336b9efb7b95bb385e8fc0e80f8b154a43
dogisgreat <github@bigly.dog>
2024-11-29 23:14:06 -0400

    ok

--- makelib/example.mk
+++ makelib/example.mk
@@ -1,5 +1,8 @@
 .PHONY: example
 
-example: ./facts/example.env
+example: ./facts/example.env ./facts/-.json ./facts/-.env
 ./facts/example.env: $(shell printf -- '%s ' ./{layers,machines}/*/usr/local/doc/examples/*.env)
 	cat -- /dev/null $^ >'$@'
+
+./facts/-.json ./facts/-.env:
+	touch -- $@
