824dc9c4cd93be7605c41624c7d12da0cad70b3d
dogisgreat <github@bigly.dog>
2025-02-04 11:42:05 -0800

    quote email

--- terraform/wtc7/mta/mda/fax.py
+++ terraform/wtc7/mta/mda/fax.py
@@ -79,7 +79,7 @@ def _redirect(msg: EmailMessage, src: str) -> Iterator[tuple[str, _Rewrite]]:
     raw_name, x_from = parseaddr(msg_from)
     name = " ".join(_decode(unquote(raw_name)))
     new_name = name + f" <{x_from}>"
-    nxt_from = formataddr((new_name, src))
+    nxt_from = formataddr((f'"{new_name}"', src))
 
     mod = {
         "from": _Rewrite(act="replace", val=nxt_from),
