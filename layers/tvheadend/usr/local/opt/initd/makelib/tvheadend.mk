bfc5caaa17f959c42e63153b2e36e5b5c53e5aa9
dogisgreat <github@bigly.dog>
2025-03-30 15:11:05 -0400

    k
    ok

--- layers/tvheadend/usr/local/opt/initd/makelib/tvheadend.mk
+++ layers/tvheadend/usr/local/opt/initd/makelib/tvheadend.mk
@@ -1,3 +1,7 @@
+pkg._: /etc/apt/sources.list.d/ppa_b-rad_kernel+mediatree+hauppauge.list
+/etc/apt/sources.list.d/ppa_b-rad_kernel+mediatree+hauppauge.list:
+	sudo -- ./libexec/add-ppa.sh 'b-rad/kernel+mediatree+hauppauge'
+
 pkg._: /etc/apt/trusted.gpg.d/tvheadend.gpg
 /etc/apt/trusted.gpg.d/tvheadend.gpg:
 	$(CURL) -- 'https://dl.cloudsmith.io/public/tvheadend/tvheadend/gpg.C6CC06BD69B430C6.key' | sudo -- gpg --batch --dearmor --yes --output '$@'
