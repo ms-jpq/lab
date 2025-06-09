fbf7d3a390a3a8ba427bb63e2374a0a2d98891a0
dogisgreat <github@bigly.dog>
2025-06-09 00:42:23 -0400

    remote debugging
    add adb

---
+++ machines/esxi/usr/local/opt/cgi/bin/youtube.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
+
+set -o pipefail
+
+if [[ ! -t 1 ]]; then
+  exec <&3 >&3
+fi
+
+BYTES=0
+while read -r LINE; do
+  LINE="${LINE%$'\r'}"
+  if [[ -z $LINE ]]; then
+    break
+  fi
+
+  LHS="${LINE%%:*}"
+  case "${LHS,,}" in
+  content-length)
+    BYTES="${LINE##*: }"
+    ;;
+  *) ;;
+  esac
+done
+
+JSON="$(head --bytes "$BYTES")"
+TARGET="$(jq --exit-status --raw-output '.target' <<< "$JSON")"
+URI="$(jq --exit-status --raw-output '.uri' <<< "$JSON")"
+
+tee <<- EOF
+HTTP/1.0 200 OK
+Content-Type: text/plain; charset=utf-8
+
+EOF
+
+CMD=(
+  am start
+  -a android.intent.action.VIEW
+  -d "$URI"
+  com.teamsmart.videomanager.tv
+)
+printf -v COMMAND -- '%q ' "${CMD[@]}"
+
+adb connect "$TARGET"
+adb shell -T "$COMMAND"
