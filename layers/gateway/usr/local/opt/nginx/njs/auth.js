7630a004a14fa007e7bdddc8287f6893e745a25c
dogisgreat <github@bigly.dog>
2025-08-03 13:26:00 -0400

    auth

---
+++ layers/gateway/usr/local/opt/nginx/njs/auth.js
@@ -0,0 +1,10 @@
+/// <reference path="../../../../../../../node_modules/njs-types/ngx_http_js_module.d.ts" />
+
+/**
+ * @param {NginxHTTPRequest} session
+ */
+const ssh_detect = (session) => {
+  session.headersIn;
+};
+
+export default { ssh_detect };
