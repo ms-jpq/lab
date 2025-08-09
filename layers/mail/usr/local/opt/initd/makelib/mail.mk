9dad23ffa3a1581436727cbf344e551be63cd13b
dogisgreat <github@bigly.dog>
2025-08-09 06:09:33 -0400

    mkdomains

--- layers/mail/usr/local/opt/initd/makelib/mail.mk
+++ layers/mail/usr/local/opt/initd/makelib/mail.mk
@@ -3,7 +3,7 @@ all: mail
 
 mail: /var/lib/local/vmail
 
-/var/lib/local/vmail:
+/var/lib/local/vmail: | pkg._
 	sudo -- mkdir -v --parents -- '$@'
 	sudo -- chown -v -- 'chasquid:chasquid' '$@'
 
@@ -13,3 +13,10 @@ mail: /usr/local/opt/apache2/apache2.conf
 
 /usr/local/opt/apache2/apache2.conf: /usr/local/opt/apache2/libexec/cfg.sed /etc/apache2/apache2.conf
 	sudo -- /usr/local/libexec/sponge2.sh '$@' '$<' /etc/apache2/apache2.conf
+
+CHASQUID_DOMAIN_DIR := /usr/local/opt/chasquid/domains/$(shell sed -E -e 's/^DOMAIN_NAME="([^"]+)"/\1/' -- /usr/local/etc/default/sieve.sh.cgi.env)
+
+mail: $(CHASQUID_DOMAIN_DIR)
+
+$(CHASQUID_DOMAIN_DIR):
+	sudo -- mkdir -p -- '$@'
