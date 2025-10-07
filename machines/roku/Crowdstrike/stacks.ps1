e2746db78e82ed10b16e97659eb881253f876723
dogisgreat <git@bigly.dog>
2025-10-07 06:32:58 -0400

    ok

--- machines/roku/Crowdstrike/stacks.ps1
+++ machines/roku/Crowdstrike/stacks.ps1
@@ -7,7 +7,6 @@ param (
 Set-StrictMode -Version 'Latest'
 $ErrorActionPreference = 'Stop'
 
-$root = Join-Path -Path $PSScriptRoot 'Compose'
 
 $network = 'traefik'
 $net = docker network ls --format 'json' | ConvertFrom-Json | Where-Object { $_.Name -eq $network }
@@ -15,9 +14,9 @@ if (-not $net) {
     docker network create --internal -- $network
 }
 
-$stacks = Get-ChildItem -Recurse -LiteralPath $root -File -Filter 'docker-compose.yml'
 
-$stacks | ForEach-Object -Parallel {
+$root = Join-Path -Path $PSScriptRoot 'Compose'
+Get-ChildItem -Recurse -LiteralPath $root -File -Filter 'docker-compose.yml' | ForEach-Object -Parallel {
     $prefix = @('compose', '--progress', 'plain', '--file')
     $argv = if ($up) {
         @($_, 'up', '--detach', '--remove-orphans')
