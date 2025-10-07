b0d5fb9ac64b270070a5a905003e7de3fbe4289e
dogisgreat <git@bigly.dog>
2025-10-07 06:31:12 -0400

    ok

--- machines/roku/Crowdstrike/compose.ps1
+++ machines/roku/Crowdstrike/compose.ps1
@@ -8,6 +8,13 @@ Set-StrictMode -Version 'Latest'
 $ErrorActionPreference = 'Stop'
 
 $root = Join-Path -Path $PSScriptRoot 'Compose'
+
+$network = 'traefik'
+$net = docker network ls --format 'json' | ConvertFrom-Json | Where-Object { $_.Name -eq $network }
+if (-not $net) {
+    docker network create --internal -- $network
+}
+
 $stacks = Get-ChildItem -Recurse -LiteralPath $root -File -Filter 'docker-compose.yml'
 
 $stacks | ForEach-Object -Parallel {
