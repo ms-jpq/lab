c1d7d13b479f25c71dcd1c11eed7de6ebd4e5096
dogisgreat <git@bigly.dog>
2025-10-07 20:17:57 -0400

    write-host

--- machines/roku/Crowdstrike/stacks.ps1
+++ machines/roku/Crowdstrike/stacks.ps1
@@ -16,14 +16,15 @@ if (-not $net) {
 
 
 $root = Join-Path -Path $PSScriptRoot 'Compose'
-Get-ChildItem -Recurse -LiteralPath $root -File -Filter 'docker-compose.yml' | ForEach-Object -Parallel {
-    $prefix = @('compose', '--progress', 'plain', '--file')
+Get-ChildItem -Recurse -LiteralPath $root -File -Filter 'docker-compose.yml' | ForEach-Object {
+    $prefix = @('compose', '--progress', 'plain', '--file', $_.FullName)
     $argv = if ($up) {
-        @($_, 'up', '--detach', '--remove-orphans')
+        @('up', '--detach', '--remove-orphans')
     }
     else {
-        @($_, 'down', '--volumes')
+        @('down', '--volumes')
     }
 
+    Write-Host -- $_.FullName
     & docker $prefix $argv
 }
