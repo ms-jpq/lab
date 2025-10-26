f93d113e0590b705bf24029a88a052c65f564626
dogisgreat <git@bigly.dog>
2025-10-26 13:48:48 -0400

    ok

--- terraform/wtc7/lambda_vars.tf
+++ terraform/wtc7/lambda_vars.tf
@@ -1,6 +1,6 @@
 locals {
+  # https://docs.aws.amazon.com/powertools/python/latest/#lambda-layer
   lambda_rt    = "python3.13"
   lambda_arch  = "arm64"
-  # https://docs.aws.amazon.com/powertools/python/latest/#lambda-layer
   lambda_layer = "arn:aws:lambda:${local.lambda_region}:017000801446:layer:AWSLambdaPowertoolsPythonV3-${replace(local.lambda_rt, ".", "")}-${local.lambda_arch}:23"
 }
