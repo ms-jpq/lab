f6b1d706275e31a6a223bde36111ccf92febeebf
dogisgreat <git@bigly.dog>
2025-10-28 10:32:30 -0400

    nocontent

--- terraform/wtc9/lambdas/ppv/routes.py
+++ terraform/wtc9/lambdas/ppv/routes.py
@@ -1,4 +1,6 @@
 from contextlib import nullcontext
+from http import HTTPStatus
+from re import compile
 
 from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
 from aws_lambda_powertools.event_handler.api_gateway import Response
@@ -9,4 +11,21 @@ with nullcontext():
 
 @app.get("/")
 def root() -> Response[None]:
-    return Response(status_code=204)
+    return Response(status_code=HTTPStatus.NO_CONTENT)
+
+
+with nullcontext():
+    _RE = compile(r"^/owncloud/\w+:/")
+
+
+@app.get("/owncloud/.+")
+def owncloud() -> Response[str]:
+    path = _RE.sub("", app.current_event.path)
+    query = app.current_event.raw_query_string
+
+    location = "https://" + path + ("?" if query else "") + query
+    return Response(
+        status_code=HTTPStatus.TEMPORARY_REDIRECT,
+        headers={"Location": location},
+        body=location,
+    )
