77baaa8a328de3e40f38da67dfaa51c09fe84782
dogisgreat <git@bigly.dog>
2025-10-29 18:06:39 -0400

    arn

--- terraform/wsb/blob_debs.tf
+++ terraform/wsb/blob_debs.tf
@@ -26,32 +26,6 @@ data "aws_iam_policy_document" "deb_bucket" {
   }
 }
 
-resource "aws_s3_bucket_website_configuration" "science_world" {
-  bucket = aws_s3_bucket.deb_bucket.bucket
-  region = aws_s3_bucket.deb_bucket.region
-  index_document {
-    suffix = "Packages"
-  }
-}
-
-data "aws_iam_policy_document" "science_world" {
-  statement {
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
-
-    actions   = ["s3:GetObject"]
-    effect    = "Allow"
-    resources = ["arn:aws:s3:::${aws_s3_bucket.deb_bucket.bucket}/*"]
-  }
-}
-
-resource "aws_s3_bucket_policy" "science_world" {
-  bucket = aws_s3_bucket.deb_bucket.bucket
-  policy = data.aws_iam_policy_document.science_world.json
-}
-
 resource "aws_cloudfront_cache_policy" "science_world" {
   name = "naive-debs"
   parameters_in_cache_key_and_forwarded_to_origin {
@@ -67,19 +41,25 @@ resource "aws_cloudfront_cache_policy" "science_world" {
   }
 }
 
+locals {
+  aws_deb_origin_id = aws_s3_bucket.deb_bucket.id
+}
+
+resource "aws_cloudfront_origin_access_control" "science_world" {
+  name                              = "science-world-tickets"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
 resource "aws_cloudfront_distribution" "science_world" {
   enabled         = true
   is_ipv6_enabled = true
 
   origin {
-    domain_name = aws_s3_bucket_website_configuration.science_world.website_endpoint
-    origin_id   = aws_s3_bucket.deb_bucket.bucket_regional_domain_name
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = []
-    }
+    domain_name              = aws_s3_bucket.deb_bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.science_world.id
+    origin_id                = local.aws_deb_origin_id
   }
   restrictions {
     geo_restriction {
@@ -90,12 +70,36 @@ resource "aws_cloudfront_distribution" "science_world" {
     allowed_methods        = ["GET", "HEAD", "OPTIONS"]
     cache_policy_id        = aws_cloudfront_cache_policy.science_world.id
     cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = aws_s3_bucket.deb_bucket.bucket_regional_domain_name
+    target_origin_id       = local.aws_deb_origin_id
     viewer_protocol_policy = "allow-all"
   }
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+}
+
+data "aws_iam_policy_document" "science_world" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions   = ["s3:GetObject"]
+    effect    = "Allow"
+    resources = ["${aws_s3_bucket.deb_bucket.arn}/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.science_world.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "science_world" {
+  bucket = aws_s3_bucket.deb_bucket.bucket
+  policy = data.aws_iam_policy_document.science_world.json
 }
 
 output "aws_debs" {
