c553fe9598e790112db11c1b03d77c9ab2afb62d
dogisgreat <git@bigly.dog>
2025-10-30 01:11:00 -0400

    emailok

--- terraform/wtc7/email_domain.tf
+++ terraform/wtc7/email_domain.tf
@@ -2,12 +2,8 @@ data "aws_route53_zone" "limited_void" {
   name = replace(regex("@.+$", var.mail_from), "@", "")
 }
 
-locals {
-  mail_subdomain = "mail"
-}
-
 resource "aws_route53_record" "limited_mx" {
-  name    = "${local.mail_subdomain}.${data.aws_route53_zone.limited_void.name}"
+  name    = "mail.${data.aws_route53_zone.limited_void.name}"
   records = ["10 inbound-smtp.${local.aws_regions.ca_c1}.amazonaws.com"]
   ttl     = local.dns_ttl
   type    = "MX"
@@ -28,7 +24,7 @@ resource "aws_route53_record" "limited_txt" {
 }
 
 resource "aws_route53_record" "_dmarc" {
-  name    = "_dmarc.${data.aws_route53_zone.limited_void.name}"
+  name    = "_dmarc.${aws_route53_record.limited_mx.name}"
   records = ["v=DMARC1;p=quarantine;rua=mailto:${local.mail_alert}"]
   ttl     = local.dns_ttl
   type    = "TXT"
@@ -55,7 +51,7 @@ resource "aws_sesv2_email_identity_mail_from_attributes" "mta" {
 
 resource "aws_route53_record" "limited_cname" {
   for_each   = toset(aws_ses_domain_dkim.limited_txt.dkim_tokens)
-  name       = "${each.key}._domainkey.${local.mail_subdomain}"
+  name       = "${each.key}._domainkey.${aws_route53_record.limited_mx.name}"
   records    = ["${each.key}.dkim.amazonses.com"]
   ttl        = local.dns_ttl
   type       = "CNAME"
