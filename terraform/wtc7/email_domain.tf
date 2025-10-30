7ccee1502c2d437591aa0b6c8da450683abf45cf
dogisgreat <git@bigly.dog>
2025-10-30 02:05:49 -0400

    domain

--- terraform/wtc7/email_domain.tf
+++ terraform/wtc7/email_domain.tf
@@ -14,17 +14,33 @@ resource "aws_sesv2_email_identity_mail_from_attributes" "mta" {
   region                 = aws_sesv2_email_identity.mta.region
 }
 
-resource "aws_route53_record" "limited_mx" {
+resource "aws_route53_record" "mx_in" {
   name    = aws_sesv2_email_identity.mta.email_identity
-  records = ["10 inbound-smtp.${local.aws_regions.ca_c1}.amazonaws.com"]
+  records = ["10 inbound-smtp.${aws_sesv2_email_identity.mta.region}.amazonaws.com"]
   ttl     = local.dns_ttl
   type    = "MX"
   zone_id = data.aws_route53_zone.limited_void.zone_id
 }
 
-resource "aws_route53_record" "_dmarc" {
+resource "aws_route53_record" "dmarc" {
   name    = "_dmarc.${aws_sesv2_email_identity.mta.email_identity}"
   records = ["v=DMARC1;p=quarantine;rua=mailto:${local.mail_alert}"]
+  ttl     = local.dns_ttl
+  type    = "TXT"
+  zone_id = data.aws_route53_zone.limited_void.zone_id
+}
+
+resource "aws_route53_record" "mx_out" {
+  name    = aws_sesv2_email_identity_mail_from_attributes.mta.mail_from_domain
+  records = ["10 feedback-smtp.${aws_sesv2_email_identity_mail_from_attributes.mta.region}.amazonses.com"]
+  ttl     = local.dns_ttl
+  type    = "MX"
+  zone_id = data.aws_route53_zone.limited_void.zone_id
+}
+
+resource "aws_route53_record" "spf" {
+  name    = aws_sesv2_email_identity_mail_from_attributes.mta.mail_from_domain
+  records = ["v=spf1 include:amazonses.com ~all"]
   ttl     = local.dns_ttl
   type    = "TXT"
   zone_id = data.aws_route53_zone.limited_void.zone_id
