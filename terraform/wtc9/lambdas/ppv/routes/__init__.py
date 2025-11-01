7ddeb15dcda6e7457cc9989f58c21d2c55a32149
dogisgreat <git@bigly.dog>
2025-11-01 12:33:22 -0400

    woah

--- terraform/wtc9/lambdas/ppv/routes/__init__.py
+++ terraform/wtc9/lambdas/ppv/routes/__init__.py
@@ -1,10 +1,27 @@
 from contextlib import nullcontext
+from urllib.parse import urlunsplit
 
 from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
+from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEventV2
 
 with nullcontext():
     app = APIGatewayHttpResolver()
 
-from . import owncloud
+
+def raw_uri(event: APIGatewayProxyEventV2) -> str:
+    uri = urlunsplit(
+        (
+            "https",
+            event.request_context.domain_name,
+            event.path,
+            event.raw_query_string,
+            "",
+        )
+    )
+    return uri
+
+
+from . import owncloud, twilio
 
 assert owncloud
+assert twilio
---
+++ terraform/wtc9/lambdas/ppv/routes/twilio.py
@@ -0,0 +1,55 @@
+from contextlib import nullcontext
+from functools import cache
+from http import HTTPStatus
+from os import environ
+
+from aws_lambda_powertools.event_handler import APIGatewayHttpResolver
+from aws_lambda_powertools.event_handler.api_gateway import Response
+from aws_lambda_powertools.event_handler.middlewares import (
+    NextMiddleware,
+)
+
+from twilio.request_validator import RequestValidator
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.voice_response import VoiceResponse
+
+from . import app, raw_uri
+
+with nullcontext():
+    _REDIRECT = environ.get("TWILIO_REDIRECT")
+
+
+@cache
+def _request_validator() -> RequestValidator:
+    return RequestValidator(token=environ[""])
+
+
+def _auth(app: APIGatewayHttpResolver, next_middleware: NextMiddleware) -> Response:
+    event = app.current_event
+    if not (signature := event.headers.get("x-twilio-signature")):
+        return Response(status_code=HTTPStatus.UNAUTHORIZED)
+
+    rv, uri = _request_validator(), raw_uri(event)
+    if not rv.validate(uri=uri, params=event.body, signature=signature):
+        return Response(status_code=HTTPStatus.FORBIDDEN)
+
+    return next_middleware.__call__(app)
+
+
+@app.get("/twilio/voice", middlewares=[_auth])
+def voice() -> Response[str]:
+    app.current_event.json_body
+
+    rsp = VoiceResponse()
+    rsp.dial(number=_REDIRECT)
+
+    return Response(status_code=HTTPStatus.OK, body=str(rsp))
+
+
+@app.get("/twilio/message", middlewares=[_auth])
+def message() -> Response[str]:
+    app.current_event.json_body
+
+    rsp = MessagingResponse()
+
+    return Response(status_code=HTTPStatus.OK, body=str(rsp))
