57060dcbe5dfd9537d9b49e7300f1a9ddde22c9c
dogisgreat <git@bigly.dog>
2025-11-02 11:17:31 -0500

    batch processpr

--- terraform/wtc9/lambdas/skyhook/__init__.py
+++ terraform/wtc9/lambdas/skyhook/__init__.py
@@ -1,18 +1,36 @@
 from contextlib import nullcontext
 from logging import INFO, captureWarnings, getLogger
 
+from aws_lambda_powertools.utilities.batch import (
+    BatchProcessor,
+    EventType,
+    process_partial_response,
+)
 from aws_lambda_powertools.utilities.batch.types import PartialItemFailureResponse
-from aws_lambda_powertools.utilities.data_classes import SQSEvent, event_source
+from aws_lambda_powertools.utilities.data_classes import (
+    SQSEvent,
+    SQSRecord,
+    event_source,
+)
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 with nullcontext():
     captureWarnings(True)
     getLogger().setLevel(INFO)
 
+with nullcontext():
+    _PROC = BatchProcessor(event_type=EventType.SQS)
+
+
+def _handler(record: SQSRecord) -> None:
+    getLogger().info("%s", record.message_id)
+
 
 @event_source(data_class=SQSEvent)
-def main(event: SQSEvent, _: LambdaContext) -> PartialItemFailureResponse:
-    for record in event.records:
-        getLogger().info("%s", record.message_id)
-
-    return {"batchItemFailures": []}
+def main(event: SQSEvent, ctx: LambdaContext) -> PartialItemFailureResponse:
+    return process_partial_response(
+        processor=_PROC,
+        event=event.raw_event,
+        context=ctx,
+        record_handler=_handler,
+    )
