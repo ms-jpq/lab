23dd71df3c351ca7fa2ae95809dcf9f3ba5a3415
dogisgreat <hao@stainlessapi.com>
2025-11-10 09:56:12 -0500

    queue

--- terraform/wtc9/lambda_queued.tf
+++ terraform/wtc9/lambda_queued.tf
@@ -6,6 +6,29 @@ resource "aws_sqs_queue" "drain" {
   region = aws_sqs_queue.sink.region
 }
 
+resource "aws_apigatewayv2_integration" "sink" {
+  api_id              = aws_apigatewayv2_api.faas.id
+  credentials_arn     = aws_iam_role.api_gateway_sqs.arn
+  integration_subtype = "SQS-SendMessage"
+  integration_type    = "AWS_PROXY"
+  region              = aws_apigatewayv2_api.faas.region
+
+  request_parameters = {
+    QueueUrl    = aws_sqs_queue.sink.id
+    MessageBody = "$request.body"
+    MessageAttributes = jsonencode({
+      Method = {
+        DataType    = "String"
+        StringValue = "$${context.httpMethod}"
+      }
+      Path = {
+        DataType    = "String"
+        StringValue = "$${request.path}"
+      }
+    })
+  }
+}
+
 resource "aws_sqs_queue_redrive_allow_policy" "pipe" {
   queue_url = aws_sqs_queue.drain.id
   region    = aws_sqs_queue.drain.region
