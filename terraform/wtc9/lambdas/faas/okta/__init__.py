909932952ca93bb8879665418b81498a3884c384
dogisgreat <hao@stainlessapi.com>
2025-11-10 10:06:39 -0500

    lambda back

---
+++ terraform/wtc9/lambdas/faas/okta/__init__.py
@@ -0,0 +1,17 @@
+from collections.abc import Mapping
+from typing import Any
+
+from aws_lambda_powertools.utilities.data_classes import event_source
+from aws_lambda_powertools.utilities.data_classes.api_gateway_authorizer_event import (
+    APIGatewayAuthorizerEventV2,
+    APIGatewayAuthorizerResponseV2,
+)
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+from .. import _
+
+
+@event_source(data_class=APIGatewayAuthorizerEventV2)
+def main(event: APIGatewayAuthorizerEventV2, _: LambdaContext) -> Mapping[str, Any]:
+
+    return APIGatewayAuthorizerResponseV2(authorize=False).asdict()
