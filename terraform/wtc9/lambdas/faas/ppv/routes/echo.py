39e1bd7023088c487f226b189402e68bf836c94e
dogisgreat <git@bigly.dog>
2025-11-18 03:06:05 -0500

    echo

--- terraform/wtc9/lambdas/faas/ppv/routes/echo.py
+++ terraform/wtc9/lambdas/faas/ppv/routes/echo.py
@@ -3,11 +3,14 @@ from logging import getLogger
 from pprint import pformat
 from typing import Any
 
-from . import app
+from . import TRACER, app
 
 
 @app.route("/echo", method=["DELETE", "GET", "HEAD", "POST", "PUT"])
 def route() -> Mapping[str, Any]:
     event = app.current_event.raw_event
-    getLogger().info("%s", pformat(event))
+    with TRACER.start_as_current_span("echo") as span:
+        getLogger().info("%s", (fmt := pformat(event)))
+        span.add_event("echoed", attributes={"pformat": fmt})
+
     return event
