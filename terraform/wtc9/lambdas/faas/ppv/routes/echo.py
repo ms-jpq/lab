557561ba1c825cd082470760484c0e4c7f5afeee
dogisgreat <git@bigly.dog>
2025-11-18 03:01:58 -0500

    kfmt

--- terraform/wtc9/lambdas/faas/ppv/routes/echo.py
+++ terraform/wtc9/lambdas/faas/ppv/routes/echo.py
@@ -1,5 +1,6 @@
 from collections.abc import Mapping
 from logging import getLogger
+from pprint import pformat
 from typing import Any
 
 from . import app
@@ -8,5 +9,5 @@ from . import app
 @app.route("/echo", method=["DELETE", "GET", "HEAD", "POST", "PUT"])
 def route() -> Mapping[str, Any]:
     event = app.current_event.raw_event
-    getLogger().info("%s", event)
+    getLogger().info("%s", pformat(event))
     return event
