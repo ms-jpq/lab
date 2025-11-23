94327056966832bc39df772df30412fc4144b2c0
dogisgreat <git@bigly.dog>
2025-11-22 21:53:27 -0500

    ok

--- terraform/wtc9/lambda_mta.tf
+++ terraform/wtc9/lambda_mta.tf
@@ -1,3 +1,44 @@
+data "aws_s3_bucket" "maildir" {
+  bucket = "kfc-mailbox"
+  region = local.lambda_region
+}
+
+data "aws_iam_policy_document" "maildir" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = [aws_sqs_queue.sink.arn]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [data.aws_s3_bucket.maildir.arn]
+    }
+  }
+}
+
+resource "aws_sqs_queue_policy" "maildir" {
+  policy    = data.aws_iam_policy_document.maildir.json
+  queue_url = aws_sqs_queue.sink.id
+  region    = aws_sqs_queue.sink.region
+}
+
+resource "aws_s3_bucket_notification" "maildir" {
+  bucket     = data.aws_s3_bucket.maildir.bucket
+  region     = aws_sqs_queue_policy.maildir.region
+
+  queue {
+    events    = ["s3:ObjectCreated:*"]
+    queue_arn = aws_sqs_queue.sink.arn
+  }
+}
+
 variable "email_alert" {
   sensitive = true
   type      = string
@@ -16,11 +57,6 @@ variable "mail_pass" {
 variable "mail_filter" {
   sensitive = true
   type      = string
-}
-
-data "aws_s3_bucket" "maildir" {
-  bucket = "kfc-mailbox"
-  region = local.lambda_region
 }
 
 data "aws_iam_policy_document" "mta" {
@@ -60,16 +96,5 @@ resource "aws_lambda_function" "mta" {
         MAIL_FILT = var.mail_filter
       }
     )
-  }
-}
-
-resource "aws_s3_bucket_notification" "maildir" {
-  depends_on = [aws_lambda_permission.faas["mta"]]
-  bucket     = data.aws_s3_bucket.maildir.bucket
-  region     = data.aws_s3_bucket.maildir.region
-
-  lambda_function {
-    events              = ["s3:ObjectCreated:*"]
-    lambda_function_arn = aws_lambda_function.mta.arn
   }
 }
