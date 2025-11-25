7e5f53d0a463b7b4b96d7d7a9cc6b949fc9769f9
dogisgreat <git@bigly.dog>
2025-11-25 00:54:07 -0500

    remove otel proxy

--- terraform/wtc9/lambdas/extensions/otlp.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env -S -- bash -Eeu -o pipefail -O dotglob -O nullglob -O extglob -O failglob -O globstar
-
-set -o pipefail
-
-cd -- "${0%/*}/../python"
-
-exec -- python3 -u -m otel_proxy
