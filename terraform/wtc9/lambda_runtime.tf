5039862e88677b1a047532d3e178be277811597d
dogisgreat <git@bigly.dog>
2025-11-27 03:22:29 -0500

    ok

--- terraform/wtc9/lambda_runtime.tf
+++ terraform/wtc9/lambda_runtime.tf
@@ -28,10 +28,11 @@ resource "aws_lambda_layer_version" "haskell" {
 locals {
   # https://docs.aws.amazon.com/powertools/python/latest/#lambda-layer
   # https://github.com/open-telemetry/opentelemetry-lambda/blob/main/python/src/otel/otel_sdk/requirements.txt
-  lambda_rt   = "python3.13"
+  lambda_rt   = "python3.14"
   lambda_arch = "arm64"
+  lambda_powertool_version = 27
   lambda_layers = [
-    "arn:aws:lambda:${local.lambda_region}:017000801446:layer:AWSLambdaPowertoolsPythonV3-${replace(local.lambda_rt, ".", "")}-${local.lambda_arch}:23",
+    "arn:aws:lambda:${local.lambda_region}:017000801446:layer:AWSLambdaPowertoolsPythonV3-${replace(local.lambda_rt, ".", "")}-${local.lambda_arch}:${local.lambda_powertool_version}",
     "arn:aws:lambda:${local.lambda_region}:184161586896:layer:opentelemetry-python-0_17_0:1",
     aws_lambda_layer_version.haskell.arn,
   ]
