191e773fafe38658d9411fb63ff28170654d3a72
dogisgreat <git@bigly.dog>
2026-01-17 23:22:22 -0500

    ok

--- layers/media/usr/local/opt/initd/makelib/media.mk
+++ layers/media/usr/local/opt/initd/makelib/media.mk
@@ -2,11 +2,11 @@
 
 all: media
 
-# media: /etc/apt/trusted.gpg.d/plex.gpg
+media: /etc/apt/trusted.gpg.d/plex.gpg
 media: /etc/apt/trusted.gpg.d/jellyfin.gpg
 
-# /etc/apt/trusted.gpg.d/plex.gpg:
-# 	$(CURL) -- 'https://downloads.plex.tv/plex-keys/PlexSign.key' | sudo -- gpg --batch --dearmor --yes --output '$@'
+/etc/apt/trusted.gpg.d/plex.gpg:
+	$(CURL) -- 'https://downloads.plex.tv/plex-keys/PlexSign.key' | sudo -- gpg --batch --dearmor --yes --output '$@'
 
 /etc/apt/trusted.gpg.d/jellyfin.gpg:
 	$(CURL) -- 'https://repo.jellyfin.org/jellyfin_team.gpg.key' | sudo -- gpg --batch --dearmor --yes --output '$@'
