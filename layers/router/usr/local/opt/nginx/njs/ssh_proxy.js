7fc5443426a0916effb91a8dbd8d16da3bcbae7f
dogisgreat <git@bigly.dog>
2026-04-26 09:57:23 -0400

    ssh

--- layers/router/usr/local/opt/nginx/njs/ssh_proxy.js
+++ layers/router/usr/local/opt/nginx/njs/ssh_proxy.js
@@ -1,16 +1,19 @@
 /// <reference path="../../../../../../../node_modules/njs-types/ngx_stream_js_module.d.ts" />
 
 const PREFIX = Buffer.from("SSH-")
+const EVENT = "upstream"
 
 export default {
   /** @param {NginxStreamRequest} session */
   detect: (session) => {
     let acc = Buffer.alloc(0)
 
-    session.on("upstream", (data) => {
+    session.on(EVENT, (data, flags) => {
       acc = Buffer.concat([acc, data])
 
-      if (acc.length >= PREFIX.length) {
+      if (flags.last || acc.length >= PREFIX.length) {
+        session.off(EVENT)
+
         const isSsh = acc.subarray(0, PREFIX.length).equals(PREFIX)
         session.variables.x_ssh_detect = isSsh ? "ssh" : "ovpn"
         session.done()
