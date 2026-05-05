e9357be0d4183fef4f05c74d76b8171dd9d220dd
dogisgreat <git@bigly.dog>
2026-05-04 20:00:55 -0400

    REMOVE irc

--- layers/irc/usr/local/bin/irc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
-
-set -o pipefail
-
-# if [[ -v TZ ]]; then
-#   printf -- '%s\n' "/set env TZ $TZ" >/run/local/weechat/weechat_fifo_*
-# fi
-
-exec -- dtach -a /run/local/weechat/dtach.sock -e '^]'
--- layers/irc/usr/local/bin/znc-conf.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env -S -- bash -Eeu -O dotglob -O nullglob -O extglob -O failglob -O globstar
-
-set -o pipefail
-
-exec -- runuser --user _znc -- znc --makeconf --datadir /var/lib/local/znc
