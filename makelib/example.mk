423e594580aef81957a48f6a8130fb745955cd0e
dogisgreat <git@bigly.dog>
2026-07-28 08:29:53 -0700

    mk

--- makelib/example.mk
+++ makelib/example.mk
@@ -2,7 +2,11 @@
 
 example: ./.facts/example.env ./.facts/-.json ./.facts/-.env
 ./.facts/example.env: $(shell printf -- '%s ' ./{layers,machines}/*/usr/local/doc/examples/*.env)
-	cat -- /dev/null $^ >'$@'
+	FILES=($^)
+	if grep --extended-regexp --line-number -v -e '^ENV_[A-Z0-9_]+=' -- "$${FILES[@]}"; then
+	  exit 1
+	fi
+	cat -- /dev/null "$${FILES[@]}" > '$@'
 
 ./.facts/-.json ./.facts/-.env:
 	touch -- $@
