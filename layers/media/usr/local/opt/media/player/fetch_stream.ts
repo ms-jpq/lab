8ca61c2d0e4bbd5a2d98a33cde91475a6c5478ee
dogisgreat <git@bigly.dog>
2026-08-28 15:06:04 -0700

    f

--- layers/media/usr/local/opt/media/player/fetch_stream.ts
+++ layers/media/usr/local/opt/media/player/fetch_stream.ts
@@ -6,8 +6,28 @@ const stream = async function* (
   using a = abortion(request.signal)
   const response = await fetch({ ...request, signal: a.signal })
 
+  if (!response.ok) {
+    throw new Error([response.status, response.statusText].join(" "))
+  }
+
   if (response.body) {
     yield* readableIterator(response.body)
   }
   return
+}
+
+export const water_stream = async function* (
+  request: Request,
+  lo: () => true,
+  hi: () => true,
+): AsyncIteratorObject<Uint8Array<ArrayBuffer>> {
+    // humm what should the logic be to support this? like we do need to stream at a different location right?
+  for (;;) {
+    for await (const bytes of stream(request)) {
+      yield bytes
+      if (hi()) {
+        break
+      }
+    }
+  }
 }
