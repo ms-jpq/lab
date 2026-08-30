071b74413b2468a60ffb1e25b059658cba8c1755
dogisgreat <git@bigly.dog>
2026-08-30 05:38:06 -0700

    k

--- makelib/compile.mk
+++ makelib/compile.mk
@@ -2,13 +2,13 @@
 
 clobber: clobber.compile
 
-TS_PROJECTS := $(patsubst ./%,%,$(shell shopt -u failglob && printf -- '%s\n' ./{layers,machines}/**/tsconfig.json))
+TS_PROJECTS := $(shell shopt -u failglob && printf -- '%s ' {layers,machines}/**/tsconfig.json)
 TS_BUILDS :=
 
 define COMPILE_TEMPLATE
 TS_BUILDS += $(dir $1)tsconfig.tsbuildinfo
 
-$(dir $1)tsconfig.tsbuildinfo: $1 tsconfig.json $(shell shopt -u failglob && printf -- '%s\n' $(dir $1)**/*.ts) | ./node_modules/.bin
+$(dir $1)tsconfig.tsbuildinfo: $1 tsconfig.json $(shell shopt -u failglob && printf -- '%s ' $(dir $1)**/*.ts) | ./node_modules/.bin
 	'./node_modules/.bin/tsc' --project '$1' --incremental --tsBuildInfoFile '$$@'
 	touch -- '$$@'
 endef
