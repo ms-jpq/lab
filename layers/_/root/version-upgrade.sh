ceb02692129d9d16fd35aec41304d764843130ec
dogisgreat <git@bigly.dog>
2026-09-07 21:03:46 -0700

    f

--- layers/_/root/version-upgrade.sh
+++ layers/_/root/version-upgrade.sh
@@ -4,19 +4,31 @@ set -o pipefail
 
 export -- DEBIAN_FRONTEND=noninteractive
 
+if ((EUID != 0)); then
+  printf -- '%s\n' 'Run this script as root.' >&2
+  exit 1
+fi
+
 if [[ ${RECUR:-} == 1 ]]; then
-  apt-get update
-  apt-get full-upgrade --assume-yes
+  apt-get update --error-on=any
+  apt-get full-upgrade --assume-yes -o APT::Get::Always-Include-Phased-Updates=true -o Dpkg::Options::=--force-confdef -o Dpkg::Options::=--force-confold
+
+  if [[ -e /run/reboot-required ]]; then
+    printf -- '%s\n' 'Preparatory updates require a reboot. Reboot, then rerun this script.' >&2
+    exit 1
+  fi
   exec -- do-release-upgrade --frontend=DistUpgradeViewNonInteractive
 fi
 
 SELF="$(realpath -- "$0")"
 SESSION='version-upgrade'
+TM=(tmux -L "$SESSION" -f /dev/null)
 
-if tmux has-session --target "$SESSION" 2> /dev/null; then
-  exec -- tmux attach-session --target "$SESSION"
+if "${TM[@]}" has-session -t "=$SESSION" 2> /dev/null; then
+  exec -- env -u TMUX "${TM[@]}" attach-session -t "=$SESSION"
 fi
 
+printf -- '%s\n' 'Before proceeding, ensure backups and a tested recovery console exist. tmux does not protect against loss of SSH access.' >&2
 printf -v COMMAND -- 'exec -- env -- RECUR=1 %q' "$SELF"
-tmux new-session --detach --session "$SESSION" "$COMMAND"
-exec -- tmux attach-session --target "$SESSION"
+"${TM[@]}" new-session -d -s "$SESSION" /bin/sh \; set-option -w -t "=$SESSION:" remain-on-exit on \; respawn-pane -k -t "=$SESSION:" "$COMMAND"
+exec -- env -u TMUX "${TM[@]}" attach-session -t "=$SESSION"
